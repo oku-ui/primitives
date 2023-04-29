f0c24fc579166508ca3aaf05b5c86f0e97755625
Fatih Yıldız <47571500+0fatihyildiz@users.noreply.github.com>
2023-04-29 21:06:48 +0300

    feat: playground nuxt3 (#48)
    * feat: playground nuxt3 (oku-ui#47)
    
    * added components
    
    * fix pnpm lock

---
+++ playground/nuxt3/app.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="w-full h-full flex items-center justify-center">
+    <NuxtPage />
+  </div>
+</template>
+
+<style>
+body {
+  margin: 0;
+}
+
+html,body, #__nuxt {
+  height: 100%;
+  font-family: sans;
+}
+</style>
---
+++ playground/nuxt3/pages/index.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+interface Page {
+  name: string
+  path: string
+}
+const pages: Page[] = [
+  {
+    name: 'OkuLabel',
+    path: '/label',
+  },
+  {
+    name: 'OkuAvatar',
+    path: '/avatar',
+  },
+  {
+    name: 'OkuAspectRatio',
+    path: '/aspect-ratio',
+  },
+]
+</script>
+
+<template>
+  <div class="grid grid-cols-3 gap-6">
+    <nuxt-link
+      v-for="page in pages" :key="page.name" :to="page.path" class="bg-cyan-900 hover:text-white text-white cursor-pointer hover:bg-cyan-700 py-4 rounded-lg px-4"
+    >
+      {{ page.name }}
+    </nuxt-link>
+  </div>
+</template>
