073caefa5b114b49025667d5aa4c1bc7490a587c
Fatih Yıldız <47571500+0fatihyildiz@users.noreply.github.com>
2023-04-29 20:44:15 +0300

    feat: playground nuxt3 (oku-ui#47)

---
+++ playground/nuxt3/app.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="h-screen w-full flex flex-col justify-center items-center">
+    <h1 class="font-sans text-cyan-900 m-0">
+      Oku UI
+    </h1>
+    <p class="text-zinc-400 text-sm font-sans">
+      Nuxt 3 playground
+    </p>
+  </div>
+</template>
