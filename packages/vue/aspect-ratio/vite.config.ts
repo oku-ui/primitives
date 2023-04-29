979880bd4fa134f4208c18a6a72b7522646dd99e
Mehmet <hi@productdevbook.com>
2023-04-29 17:56:51 +0300

    feat: aspect-ratio (#43)
    * feat: aspect-ratio
    
    * fix: vite build

--- packages/vue/aspect-ratio/vite.config.ts
+++ packages/vue/aspect-ratio/vite.config.ts
@@ -2,6 +2,7 @@ import path, { resolve } from 'node:path'
 
 import Vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
+
 // https://github.com/qmhc/vite-plugin-dts
 import dtsPlugin from 'vite-plugin-dts'
 
@@ -35,7 +36,6 @@ export default defineConfig({
     },
   },
   build: {
-    target: 'modules',
     lib: {
       entry: path.resolve(__dirname, './src/index.ts'),
       formats: ['es', 'cjs'],
