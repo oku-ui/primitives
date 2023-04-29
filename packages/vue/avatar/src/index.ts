0f35c47bcee79cfb1ceea96c227a231575d7e1bc
productdevbook <hi@productdevbook.com>
2023-04-29 19:03:36 +0300

    feat: avatar

---
+++ packages/vue/avatar/src/index.ts
@@ -0,0 +1,7 @@
+export {
+  OkuAvatar,
+  OkuAvatarFallback,
+  OkuAvatarImage,
+  createAvatarScope,
+} from './avatar'
+export type { AvatarFallbackProps, AvatarProps, AvatarImageProps } from './avatar'
