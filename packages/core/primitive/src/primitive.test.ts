8452fe29d288a41ee5f6c3824a748e712c631a8b
Clarance Liberiste Ntwari <liberintwari@gmail.com>
2023-04-30 11:36:44 +0200

    test: advanced anchor test

--- packages/core/primitive/src/primitive.test.ts
+++ packages/core/primitive/src/primitive.test.ts
@@ -8,4 +8,19 @@ describe('Primitive', () => {
     expect(wrapper.exists()).toBe(true)
     expect(wrapper.find('div').exists()).toBe(true)
   })
+
+  it('renders an anchor with href attribute', () => {
+    const wrapper = mount(Primitive.a, {
+      attrs: {
+        href: 'https://example.com',
+      },
+      slots: {
+        default: 'Visit Example.com',
+      },
+    })
+
+    expect(wrapper.element.tagName).toBe('A')
+    expect(wrapper.attributes('href')).toBe('https://example.com')
+    expect(wrapper.text()).toBe('Visit Example.com')
+  })
 })
