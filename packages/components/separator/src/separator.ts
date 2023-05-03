e0f41723f2baf73b228cf582a843fcf04e2f2153
Victor Akintunde <akintoluvic@gmail.com>
2023-05-03 10:54:35 +0100

    prop types

--- packages/components/separator/src/separator.ts
+++ packages/components/separator/src/separator.ts
@@ -1,13 +1,29 @@
-import { defineComponent, h, ref } from "vue"
+import { defineComponent, h, ref } from 'vue'
 import type { ComponentPropsWithoutRef, ElementRef } from '@oku-ui/primitive'
-import { Primitive } from "@oku-ui/primitive"
+import { Primitive } from '@oku-ui/primitive'
 
 type PrimitiveSeparatorProps = ComponentPropsWithoutRef<typeof Primitive.div>
 
-interface SeparatorProps extends PrimitiveSeparatorProps {}
-type SeparatorElement = ElementRef<typeof Primitive.div>
+// interface SeparatorProps extends PrimitiveSeparatorProps {}
+// type SeparatorElement = ElementRef<typeof Primitive.div>
 
-const NAME = "Separator"
+const NAME = 'Separator'
+const DEFAULT_ORIENTATION = 'horizontal'
+const ORIENTATIONS = ['horizontal', 'vertical'] as const
+
+type Orientation = typeof ORIENTATIONS[number]
+type SeparatorElement = ElementRef<typeof Primitive.div>
+interface SeparatorProps extends PrimitiveSeparatorProps {
+  /**
+   * Either `vertical` or `horizontal`. Defaults to `horizontal`.
+   */
+  orientation?: Orientation
+  /**
+   * Whether or not the component is purely decorative. When true, accessibility-related attributes
+   * are updated so that that the rendered element is removed from the accessibility tree.
+   */
+  decorative?: boolean
+}
 
 const Separator = defineComponent<SeparatorProps>({
   name: NAME,
