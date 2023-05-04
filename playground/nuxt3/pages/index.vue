5184a3f213cc967fe11c81687344750c4c70fdd9
Victor Akintunde <akintoluvic@gmail.com>
2023-05-04 10:30:26 +0100

    separator nuxt play

--- playground/nuxt3/pages/index.vue
+++ playground/nuxt3/pages/index.vue
@@ -1,28 +1,35 @@
 <script setup lang="ts">
 interface Page {
-  name: string
-  path: string
+  name: string;
+  path: string;
 }
 const pages: Page[] = [
   {
-    name: 'OkuLabel',
-    path: '/label',
+    name: "OkuLabel",
+    path: "/label",
   },
   {
-    name: 'OkuAvatar',
-    path: '/avatar',
+    name: "OkuAvatar",
+    path: "/avatar",
   },
   {
-    name: 'OkuAspectRatio',
-    path: '/aspect-ratio',
+    name: "OkuAspectRatio",
+    path: "/aspect-ratio",
   },
-]
+  {
+    name: "OkuSeparator",
+    path: "/separator",
+  },
+];
 </script>
 
 <template>
   <div class="grid grid-cols-3 gap-6">
     <nuxt-link
-      v-for="page in pages" :key="page.name" :to="page.path" class="bg-cyan-900 hover:text-white text-white cursor-pointer hover:bg-cyan-700 py-4 rounded-lg px-4"
+      v-for="page in pages"
+      :key="page.name"
+      :to="page.path"
+      class="bg-cyan-900 hover:text-white text-white cursor-pointer hover:bg-cyan-700 py-4 rounded-lg px-4"
     >
       {{ page.name }}
     </nuxt-link>
