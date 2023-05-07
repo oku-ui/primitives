ead96d8fcf83c250499e377b3c93cda74d9699cc
Mehmet <hi@productdevbook.com>
2023-05-07 06:51:41 +0300

    chore: auto import component nuxt playground (#91)
    * chore: auto import component nuxt playground
    
    * [autofix.ci] apply automated fixes

--- playground/nuxt3/nuxt.config.ts
+++ playground/nuxt3/nuxt.config.ts
@@ -1,16 +1,22 @@
-import { resolve } from 'node:path'
+import { addComponent } from '@nuxt/kit'
+import { globbySync } from 'globby'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: [
+    (_, nuxt) => {
+      const components = globbySync('../../packages/components/**/src/stories/*.@(vue)')
+      if (components.length > 0) {
+        components.forEach((component) => {
+          // LabelDemo.vue -> LabelDemo
+          addComponent({ filePath: component, name: component.split('/').pop()!.replace('.vue', '') })
+        })
+      }
+    },
     '@unocss/nuxt',
   ],
   css: [
     '@unocss/reset/tailwind.css',
   ],
-  components: {
-    dirs: [
-      resolve(__dirname, './../../packages/components/label/src/stories'),
-    ],
-  },
+
 })
