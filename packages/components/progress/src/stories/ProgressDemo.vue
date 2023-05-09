5d765e9c7abe7f29bcc240ffe62a5e80c724086f
YeSuX <351490949@qq.com>
2023-05-09 13:47:24 +0800

    feat: add progress styled story

--- packages/components/progress/src/stories/ProgressDemo.vue
+++ packages/components/progress/src/stories/ProgressDemo.vue
@@ -2,7 +2,7 @@
 <script setup lang="ts">
 import type { ProgressProps } from '@oku-ui/progress'
 import { OkuProgress, OkuProgressIndicator } from '@oku-ui/progress'
-import { computed, ref, watch } from 'vue'
+import { computed, ref } from 'vue'
 
 export interface OkuProgressProps extends ProgressProps {
   template?: '#1' | '#2'
@@ -16,15 +16,22 @@ withDefaults(defineProps<OkuProgressProps>(), {
 
 // #1
 const max = 150
-const value = ref(13)
+const value = ref<number | null>(13)
+const valueCopy = ref<number | null>(null)
 const percentage = computed(() => value.value != null ? Math.round((value.value / max) * 100) : null)
 
 console.log(typeof value.value)
 
-watch(value, (newvalue) => {
-  console.log(typeof value.value)
-  console.log(typeof newvalue)
-})
+function toggleIndeterminate() {
+  if (value.value === null) {
+    value.value = valueCopy.value
+    valueCopy.value = null
+  }
+  else {
+    valueCopy.value = value.value
+    value.value = null
+  }
+}
 </script>
 
 <template>
@@ -32,13 +39,26 @@ watch(value, (newvalue) => {
     <div v-if="template === '#1' || allshow" class="flex flex-col">
       <OkuProgress class="w-400px h-25px max-w-full border-5 border-black bg-gray-200" :value="value" :max="max">
         <OkuProgressIndicator
-          class="w-0 h-full bg-red transition duration-150 ease-out"
+          class="indicator w-0 h-full transition duration-150 ease-out"
           :style="{ width: percentage != null ? `${percentage}%` : undefined }"
         />
       </OkuProgress>
       {{ value }}
-      <input v-model.number="value" type="range" min="0" :max="max">
-      <input v-model="value" type="number">
+      <br>
+      <button @click="toggleIndeterminate">
+        Toggle Indeterminate
+      </button>
+      <input v-model.number="value" type="range" min="0" :max="max" :disabled="value === null">
     </div>
   </div>
 </template>
+
+<style scoped>
+.indicator[data-state="complete"]{
+  background-color: green;
+}
+
+.indicator[data-state="loading"]{
+  background-color: gray;
+}
+</style>
