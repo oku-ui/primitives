e1ad1e4d2ee0e26dc9584eb7e7b90c695e9c717e
productdevbook <hi@productdevbook.com>
2023-05-28 15:58:12 +0300

    types(separator): type designation correction

--- packages/components/separator/src/separator.ts
+++ packages/components/separator/src/separator.ts
@@ -27,15 +27,15 @@ const Separator = defineComponent({
   inheritAttrs: false,
   props: {
     /**
-     * Either `vertical` or `horizontal`. Defaults to `horizontal`.
+    * Whether or not the component is purely decorative. When true, accessibility-related attributes
+    * are updated so that that the rendered element is removed from the accessibility tree.
     */
     decorative: {
       type: Boolean,
       default: false,
     },
     /**
-     * Whether or not the component is purely decorative. When true, accessibility-related attributes
-     * are updated so that that the rendered element is removed from the accessibility tree.
+     * Either `vertical` or `horizontal`. Defaults to `horizontal`.
      */
     orientation: {
       type: String as PropType<Orientation>,
