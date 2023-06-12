141e2a3c23a1fd6d3c0e6296515eb8429811d1c4
suxiong <44074974+YeSuX@users.noreply.github.com>
2023-06-13 01:05:10 +0800

    fix(use-composable): useId bug (#162)
    * feat: add useId
    
    * fix: useId bug

--- packages/core/use-composable/src/useId.ts
+++ packages/core/use-composable/src/useId.ts
@@ -1,14 +1,12 @@
-import { onMounted, ref } from 'vue'
+import { ref } from 'vue'
 
 let count = 0
 
 function useId(deterministicId?: string): string {
   const id = ref<string | undefined>()
 
-  onMounted(() => {
-    if (!deterministicId)
-      id.value = id.value ?? String(count++)
-  })
+  if (!deterministicId)
+    id.value = id.value ?? String(count++)
 
   return deterministicId || (id.value ? `oku-${id.value}` : '')
 }
