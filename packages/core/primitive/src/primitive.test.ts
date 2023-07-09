767d9b8c8e5acb5f984bd267ee33b2fba375e53f
Damilare Anjorin <damilareanjorin1@gmail.com>
2023-07-09 05:49:31 +0100

    chore: add more primitive tests

--- packages/core/primitive/src/primitive.test.ts
+++ packages/core/primitive/src/primitive.test.ts
@@ -144,4 +144,108 @@ describe('Primitive', () => {
     expect(element.exists()).toBe(true);
     expect(element.element.value).toBe(value);
   });
+
+  it('emits focus and blur events on input element', async () => {
+    const wrapper = mount(Primitive.input);
+    const element = wrapper.find('input');
+
+    await element.trigger('focus');
+    expect(wrapper.emitted('focus')).toBeTruthy();
+
+    await element.trigger('blur');
+    expect(wrapper.emitted('blur')).toBeTruthy();
+  });
+
+  it('renders input element with type "email"', () => {
+    const wrapper = mount(Primitive.input, {
+      props: {
+        type: 'email',
+      },
+    });
+
+    const element = wrapper.find('input');
+    expect(element.exists()).toBe(true);
+    expect(element.attributes('type')).toBe('email');
+  });
+
+  it('renders input element with required attribute', () => {
+    const wrapper = mount(Primitive.input, {
+      props: {
+        required: true,
+      },
+    });
+
+    const element = wrapper.find('input');
+    expect(element.exists()).toBe(true);
+    expect(element.element.required).toBe(true);
+  });
+
+  it('renders anchor element with href attribute', () => {
+    const href = 'https://example.com';
+    const wrapper = mount(Primitive.a, {
+      props: {
+        href: href,
+      },
+    });
+
+    const element = wrapper.find('a');
+    expect(element.exists()).toBe(true);
+    expect(element.attributes('href')).toBe(href);
+  });
+
+  it('renders anchor element with target="_blank" attribute', () => {
+    const wrapper = mount(Primitive.a, {
+      props: {
+        href: 'https://example.com',
+        target: '_blank',
+      },
+    });
+
+    const element = wrapper.find('a');
+    expect(element.exists()).toBe(true);
+    expect(element.attributes('target')).toBe('_blank');
+  });
+
+  it('renders anchor element with rel attribute', () => {
+    const rel = 'noopener noreferrer';
+    const wrapper = mount(Primitive.a, {
+      props: {
+        href: 'https://example.com',
+        rel: rel,
+      },
+    });
+
+    const element = wrapper.find('a');
+    expect(element.exists()).toBe(true);
+    expect(element.attributes('rel')).not.toBe('opener');
+    expect(element.attributes('rel')).toBe(rel);
+  });
+
+  it('renders anchor element with text content', () => {
+    const textContent = 'Click here';
+    const wrapper = mount(Primitive.a, {
+      props: {
+        href: 'https://example.com',
+      },
+      slots: {
+        default: textContent,
+      },
+    });
+
+    const element = wrapper.find('a');
+    expect(element.exists()).toBe(true);
+    expect(element.text()).toBe(textContent);
+  });
+
+  it('emits click event when anchor element is clicked', async () => {
+    const wrapper = mount(Primitive.a, {
+      props: {
+        href: 'https://example.com',
+      },
+    });
+
+    const element = wrapper.find('a');
+    await element.trigger('click');
+    expect(wrapper.emitted('click')).toBeTruthy();
+  });
 });
