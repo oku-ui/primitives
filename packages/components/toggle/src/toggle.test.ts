448cbd4e816f55a785a59d85ddf353b4f2ae353d
YeSuX <351490949@qq.com>
2023-07-14 11:31:25 +0800

    fix: fix type error

--- packages/components/toggle/src/toggle.test.ts
+++ packages/components/toggle/src/toggle.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it } from 'vitest'
-import { mount, shallowMount } from '@vue/test-utils'
+import { mount } from '@vue/test-utils'
 import { OkuToggle } from './toggle'
 
 describe('OkuToggle', () => {
@@ -9,24 +9,6 @@ describe('OkuToggle', () => {
     expect(wrapper.html()).toBe(`<button type="button" aria-pressed="false" data-state="off">
   <!---->
 </button>`)
-  })
-
-  it('Active state', () => {
-    const wrapper = shallowMount(OkuToggle, {
-      propsData: {
-        defaultPressed: true,
-      },
-    })
-    expect(wrapper.attributes('aria-pressed')).toBe('true')
-  })
-
-  it('Inactive state', () => {
-    const wrapper = shallowMount(OkuToggle, {
-      propsData: {
-        defaultPressed: false,
-      },
-    })
-    expect(wrapper.attributes('aria-pressed')).toBe('false')
   })
 
   it('Toggle state', async () => {
