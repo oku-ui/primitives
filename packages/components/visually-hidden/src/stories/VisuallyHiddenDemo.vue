81e462facb5b3787550253f4bb22fa0f72a700b2
Damilare Anjorin <damilareanjorin1@gmail.com>
2023-07-22 11:55:56 +0100

    feat(visually-hidden): add visually hidden component

---
+++ packages/components/visually-hidden/src/stories/VisuallyHiddenDemo.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import type { VisuallyHiddenProps } from '@oku-ui/visually-hidden'
+import { OkuVisuallyHidden } from '@oku-ui/visually-hidden'
+
+export interface IVisuallyHiddenProps extends VisuallyHiddenProps {
+  template?: '#1'
+  allshow?: boolean
+}
+
+defineProps<IVisuallyHiddenProps>()
+</script>
+
+<template>
+  <button>
+    <OkuVisuallyHidden>Save the file</OkuVisuallyHidden>
+    <span aria-hidden>💾</span>
+  </button>
+</template>
