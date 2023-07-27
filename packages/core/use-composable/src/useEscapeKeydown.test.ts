b971406ef3c413547242848169c0c3637b12b10e
Cr0zy07 <49247629+Cr0zy07@users.noreply.github.com>
2023-07-28 00:05:26 +0800

    chore(use-composable): add new useEscapeKeydown (#248)
    * feat: add useEscapeKeydown
    
    * add a cleanup callback
    
    * add test
    
    * cleanup

---
+++ packages/core/use-composable/src/useEscapeKeydown.test.ts
@@ -0,0 +1,34 @@
+import { beforeEach, describe, expect, it, vi } from 'vitest'
+import { useEscapeKeydown } from './useEscapeKeydown'
+
+describe('useEscapeKeydown', () => {
+  let onEscapeKeyDown: any
+  let ownerDocument: Document
+
+  beforeEach(() => {
+    onEscapeKeyDown = vi.fn()
+    ownerDocument = globalThis.document
+  })
+
+  it('should call onEscapeKeyDown when the escape key is pressed', () => {
+    onEscapeKeyDown = vi.fn()
+    ownerDocument = globalThis.document
+
+    useEscapeKeydown(onEscapeKeyDown, ownerDocument)
+
+    const escapeKeyEvent = new KeyboardEvent('keydown', { key: 'Escape' })
+    ownerDocument.dispatchEvent(escapeKeyEvent)
+
+    expect(onEscapeKeyDown).toHaveBeenCalledTimes(1)
+    expect(onEscapeKeyDown).toHaveBeenCalledWith(escapeKeyEvent)
+  })
+
+  it('should not call onEscapeKeyDown for other key presses', () => {
+    useEscapeKeydown(onEscapeKeyDown, ownerDocument)
+
+    const otherKeyEvent = new KeyboardEvent('keydown', { key: 'Enter' })
+    ownerDocument.dispatchEvent(otherKeyEvent)
+
+    expect(onEscapeKeyDown).not.toHaveBeenCalled()
+  })
+})
