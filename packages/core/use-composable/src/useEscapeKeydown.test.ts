73300bef4d759302061cbc1966ffa8107c96e59a
Cr0zy07 <49247629+Cr0zy07@users.noreply.github.com>
2023-07-30 07:40:56 +0800

    refactor(useEscapeKeydown): cleanup test (#250)

--- packages/core/use-composable/src/useEscapeKeydown.test.ts
+++ packages/core/use-composable/src/useEscapeKeydown.test.ts
@@ -1,24 +1,30 @@
 import { beforeEach, describe, expect, it, vi } from 'vitest'
+import type { SpyInstance } from 'vitest'
+import { mount } from '@vue/test-utils'
 import { useEscapeKeydown } from './useEscapeKeydown'
 
 describe('useEscapeKeydown', () => {
   let onEscapeKeyDown: any
   let ownerDocument: Document
+  let addSpy: SpyInstance
+  let removeSpy: SpyInstance
 
   beforeEach(() => {
     onEscapeKeyDown = vi.fn()
     ownerDocument = globalThis.document
+    addSpy = vi.spyOn(ownerDocument, 'addEventListener')
+    removeSpy = vi.spyOn(ownerDocument, 'removeEventListener')
   })
 
   it('should call onEscapeKeyDown when the escape key is pressed', () => {
-    onEscapeKeyDown = vi.fn()
-    ownerDocument = globalThis.document
-
     useEscapeKeydown(onEscapeKeyDown, ownerDocument)
+
+    expect(removeSpy).not.toHaveBeenCalled()
 
     const escapeKeyEvent = new KeyboardEvent('keydown', { key: 'Escape' })
     ownerDocument.dispatchEvent(escapeKeyEvent)
 
+    expect(addSpy).toHaveBeenCalledTimes(1)
     expect(onEscapeKeyDown).toHaveBeenCalledTimes(1)
     expect(onEscapeKeyDown).toHaveBeenCalledWith(escapeKeyEvent)
   })
@@ -26,9 +32,32 @@ describe('useEscapeKeydown', () => {
   it('should not call onEscapeKeyDown for other key presses', () => {
     useEscapeKeydown(onEscapeKeyDown, ownerDocument)
 
+    expect(removeSpy).not.toHaveBeenCalled()
+
     const otherKeyEvent = new KeyboardEvent('keydown', { key: 'Enter' })
     ownerDocument.dispatchEvent(otherKeyEvent)
 
+    expect(addSpy).toHaveBeenCalledTimes(1)
     expect(onEscapeKeyDown).not.toHaveBeenCalled()
+    expect(onEscapeKeyDown).not.toHaveBeenCalledWith(otherKeyEvent)
+  })
+
+  it('should remove event listener when the component unmounts', () => {
+    const wrapper = mount({
+      template: '<div></div>',
+      setup() {
+        useEscapeKeydown(onEscapeKeyDown, ownerDocument)
+      },
+    })
+
+    expect(removeSpy).not.toHaveBeenCalled()
+
+    wrapper.trigger('keydown', { key: 'Escape' })
+
+    expect(addSpy).toBeCalledTimes(1)
+
+    wrapper.unmount()
+
+    expect(removeSpy).toHaveBeenCalledTimes(1)
   })
 })
