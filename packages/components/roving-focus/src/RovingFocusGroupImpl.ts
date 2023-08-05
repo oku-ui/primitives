0ed9cfe230e43da39a0b9a17cf0d74433e66aa18
productdevbook <hi@productdevbook.com>
2023-08-05 06:35:00 +0300

    feat: usecomposeEventHanders

--- packages/components/roving-focus/src/RovingFocusGroupImpl.ts
+++ packages/components/roving-focus/src/RovingFocusGroupImpl.ts
@@ -1,14 +1,15 @@
 import type { PropType } from 'vue'
 import { computed, defineComponent, h, ref, toRefs, watchEffect } from 'vue'
-import { useCallbackRef, useComposedRefs, useControllable, useForwardRef } from '@oku-ui/use-composable'
+import { useCallbackRef, useComposeEventHandlers, useComposedRefs, useControllable, useForwardRef } from '@oku-ui/use-composable'
 
 import type { ComponentPublicInstanceRef, ElementType } from '@oku-ui/primitive'
 
 import { Primitive } from '@oku-ui/primitive'
-import type { Direction, Orientation } from './utils'
+import { type Direction, type Orientation, focusFirst } from './utils'
 import { RovingFocusProvider, ScopedProps, useCollection } from './RovingFocusGroup'
 
 const ENTRY_FOCUS = 'rovingFocusGroup.onEntryFocus'
+const EVENT_OPTIONS = { bubbles: false, cancelable: true }
 
 export type RovingFocusGroupImplElement = ElementType<'div'>
 export type _RovingFocusGroupImplEl = HTMLDivElement
@@ -63,7 +64,7 @@ const OkuRovingFocusGroupImpl = defineComponent({
     entryFocus: (event: Event) => true,
   },
   setup(props, { attrs, slots, emit }) {
-    const _attrs = attrs as Omit<RovingFocusGroupImplElement, 'dir'>
+    const _attrs = attrs as Omit<_RovingFocusGroupImplEl, 'dir'>
     const {
       orientation,
       loop,
@@ -78,7 +79,7 @@ const OkuRovingFocusGroupImpl = defineComponent({
     const forwardedRef = useForwardRef()
     const composedRefs = useComposedRefs(buttonRef, forwardedRef)
 
-    const { state, updateValue: updateCurrentTabStopId } = useControllable({
+    const { state: currentTabStopId, updateValue: updateCurrentTabStopId } = useControllable({
       prop: computed(() => currentTabStopIdProp.value),
       defaultProp: computed(() => defaultCurrentTabStopId.value),
       onChange: (result: any) => {
@@ -106,7 +107,7 @@ const OkuRovingFocusGroupImpl = defineComponent({
       orientation: orientation.value,
       dir: dir.value,
       loop: loop.value ?? false,
-      currentTabStopId: state.value ?? null,
+      currentTabStopId: currentTabStopId.value ?? null,
       onItemFocus: (tabStopId) => {
         updateCurrentTabStopId(tabStopId)
       },
@@ -124,6 +125,44 @@ const OkuRovingFocusGroupImpl = defineComponent({
     return () => h(Primitive.div, {
       'tabIndex': isTabbingBackOut.value || focusableItemsCount.value === 0 ? -1 : 0,
       'data-orientation': orientation.value,
+      'ref': composedRefs,
+      'style': {
+        outline: 'none',
+        ..._attrs.style,
+      },
+      'onMouseDown': () => {
+        useComposeEventHandlers(_attrs.onmousedown, () => {
+          isClickFocusRef.value = true
+        })
+      },
+      'onFocus': () => {
+        useComposeEventHandlers(_attrs.onfocus, (event) => {
+          // We normally wouldn't need this check, because we already check
+          // that the focus is on the current target and not bubbling to it.
+          // We do this because Safari doesn't focus buttons when clicked, and
+          // instead, the wrapper will get focused and not through a bubbling event.
+          const isKeyboardFocus = !isClickFocusRef.value
+
+          if (event.target === event.currentTarget && isKeyboardFocus && !isTabbingBackOut.value) {
+            const entryFocusEvent = new CustomEvent(ENTRY_FOCUS, EVENT_OPTIONS)
+            event.currentTarget?.dispatchEvent(entryFocusEvent)
+
+            if (!entryFocusEvent.defaultPrevented) {
+              const items = getItems.value.filter(item => item.focusable)
+              const activeItem = items.find(item => item.active)
+              const currentItem = items.find(item => item.id === currentTabStopId.value)
+              const candidateItems = [activeItem, currentItem, ...items].filter(
+                Boolean,
+              ) as typeof items
+              const candidateNodes = candidateItems.map(item => item.ref.value!)
+              focusFirst(candidateNodes)
+            }
+          }
+
+          isClickFocusRef.value = false
+        })
+      },
+
     })
   },
 })
