b99fa63bf6b22d4a47a5812e4cd65625f8564948
Ramzo <frist.r5vzn@gmail.com>
2023-08-06 03:33:18 +0400

    direction component unit test

---
+++ packages/components/direction/src/direction.test.ts
@@ -0,0 +1,61 @@
+import { describe, expect, it } from 'vitest'
+import { mount } from '@vue/test-utils'
+import { type Component } from 'vue'
+import { DirectionContextSymbol, DirectionProvider, useDirection } from './Direction.ts'
+
+describe('direction', () => {
+  function propsTest(dir: string) {
+    it(`props ${dir}`, () => {
+      const wrapper = mount(DirectionProvider, {
+        props: {
+          dir,
+        },
+      })
+      expect(wrapper.props().dir).toBe(dir)
+    })
+  }
+
+  propsTest('ltr')
+  propsTest('rtl')
+
+  it('slot', () => {
+    const wrapper = mount(DirectionProvider, {
+      slots: {
+        default: 'test',
+      },
+    })
+    expect(wrapper.html()).toContain('test')
+  })
+  it('provide, inject and slot', () => {
+    const ChildComponent = {
+      setup() {
+        const dir = useDirection()
+        return {
+          dir,
+        }
+      },
+      template: '<div>{{ dir }}</div>',
+    } as Component
+
+    const childWrapper = mount(ChildComponent, {
+      global: {
+        provide: {
+          [DirectionContextSymbol]: 'ltr',
+        },
+      },
+    })
+
+    const main = {
+      components: { DirectionProvider, ChildComponent },
+      template: `
+          <DirectionProvider dir="ltr">
+            <ChildComponent />
+          </DirectionProvider>
+        `,
+    }
+
+    const wrapper = mount(main, {})
+    expect(wrapper.html()).toContain('ltr')
+    expect(childWrapper.html()).toContain('ltr')
+  })
+})
