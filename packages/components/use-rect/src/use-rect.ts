8483263850addfde0e869b8d32c8c5244ec673fe
Ramzo <frist.r5vzn@gmail.com>
2023-08-08 22:26:14 +0400

    Moved into measurableElement and rect function

--- packages/components/use-rect/src/use-rect.ts
+++ packages/components/use-rect/src/use-rect.ts
@@ -2,10 +2,9 @@ import { onBeforeUnmount, onMounted, ref } from 'vue'
 import { observeElementRect } from '@oku-ui/utils'
 import type { Measurable } from '@oku-ui/utils'
 
-const measurableElement = ref<Measurable | null>(null)
-const rect = ref<DOMRectReadOnly | null>(null)
-
 function useRect() {
+  const measurableElement = ref<Measurable | null>(null)
+  const rect = ref<DOMRectReadOnly | null>(null)
   onMounted(() => {
     if (measurableElement.value) {
       const unobserve = observeElementRect(measurableElement.value, (newRect) => {
