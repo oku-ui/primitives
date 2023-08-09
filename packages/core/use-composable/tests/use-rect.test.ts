391f9029aeba1980a1cd05fd97c136b4e26ebdfb
Ramzo <frist.r5vzn@gmail.com>
2023-08-09 16:30:34 +0400

    rect test completed

--- packages/core/use-composable/tests/use-rect.test.ts
+++ packages/core/use-composable/tests/use-rect.test.ts
@@ -1,52 +1,26 @@
 import { mount } from '@vue/test-utils'
-import { describe, expect, it, vitest } from 'vitest'
-import { ref } from 'vue'
+import { describe, expect, it } from 'vitest'
 import { useRect } from '../src/use-rect'
 
 describe('useRect', () => {
-  it('rect changes', async () => {
-    const element = ref<HTMLElement | null>(document.createElement('div'))
-    const { rect, measurableElement } = useRect()
-
-    measurableElement.value = element.value || undefined
+  it('rect', async () => {
+    const { rect } = useRect()
     expect(rect.value).toBeNull()
-
-    // Mock appendChild and remove methods
-    document.body.appendChild = vitest.fn()
-    element.value!.remove = vitest.fn()
-
-    await mount({
-      template: '<div></div>',
-      setup() {
-        measurableElement.value = element.value
-        return {}
-      },
-    })
-
-    expect(rect.value).not.toBeNull()
-
-    // Simulate component unmounting
-    await mount({
-      template: '<div></div>',
-      setup() {
-        measurableElement.value = null
-        return {}
-      },
-    })
-
-    expect(rect.value).toBeNull()
-
-    // Assert on the number of calls
-    expect(document.body.appendChild).toHaveBeenCalledTimes(1)
-    expect(element.value!.remove).toHaveBeenCalledTimes(1)
   })
 
   it('props', () => {
-    const wrapper = mount(useRect, {
-      propsData: {
-        dir: 'measurableElement',
+    const { measurableElement } = useRect()
+    expect(measurableElement.value).toBeNull()
+
+    const wrapper = mount({
+      template: '<div></div>',
+      setup() {
+        return {
+          dir: measurableElement,
+        }
       },
     })
-    expect(wrapper.props().dir).toBe('measurableElement')
+
+    expect(wrapper.vm.dir).toBeNull()
   })
 })
