962561b20bf7724cdc337a1411776d867e06bc3c
productdevbook <hi@productdevbook.com>
2023-08-12 20:22:24 +0300

    fix: dev build mode

--- scripts/dev.ts
+++ scripts/dev.ts
@@ -13,8 +13,8 @@ const packages = globbySync(resolve('packages/components'), {
   onlyDirectories: true,
   deep: 1,
   ignore: [
-    '**/node_modules',
-    '**/dist',
+    '**/node_modules/**',
+    '**/dist/**',
   ],
 })
 
@@ -40,40 +40,47 @@ async function deleteAllPackageDists() {
   execSync('pnpm build', { stdio: 'inherit' })
   console.log('finished building components ✅')
 
-  console.log('watch mode active 🚀')
   watchMode()
 }
 
 await deleteAllPackageDists()
 
-function whereComponent(path: string) {
+async function whereComponent(path: string) {
+  console.log('starting build 🏗')
   const isComponent = path.includes(join('packages/components'))
   const isCore = path.includes(join('packages/core'))
 
   if (isComponent) {
     const temp = path.slice(componentsPath.length, path.length)
-    console.time()
     execSync(`pnpm --filter @oku-ui/${dirname(pjoin(temp)).split('/')[1]} run build`, { stdio: 'inherit' })
-    console.timeEnd()
   }
 
   if (isCore) {
     const temp = path.slice(corePath.length, path.length)
-    console.time()
     execSync(`pnpm --filter @oku-ui/${dirname(pjoin(temp)).split('/')[1]} run build`, { stdio: 'inherit' })
-    console.timeEnd()
   }
+  console.log('finished build ✅')
 }
 
 function watchMode() {
   watch(resolve('packages'), {
     ignored: [
       '/(^|[\/\\])\../', // ignore dotfiles
-      '**/node_modules',
-      '**/dist',
+      '**/node_modules/**',
+      '**/dist/**',
     ],
     ignoreInitial: true,
-  }).on('all', (event, path) => {
-    whereComponent(path)
+  }).on('all', async (event, path) => {
+    await whereComponent(path)
+  }).once('ready', () => {
+    console.log('watch mode active 🚀')
+  }).prependListener('unlinkDir', async (path) => {
+    console.log('deleting dist folders 🔥')
+    for await (const path of packages)
+      rimrafSync(`${path}/dist`)
+
+    for await (const path of core)
+      rimrafSync(`${path}/dist`)
+    console.log('finished deleting dist folders ✅')
   })
 }
