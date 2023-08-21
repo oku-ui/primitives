8e3308671b36bf0c2fbd599797c9ab6ee4bd0bb2
productdevbook <hi@productdevbook.com>
2023-08-21 21:01:53 +0300

    fix: test

--- packages/components/radio-group/tests/radio.test.ts
+++ packages/components/radio-group/tests/radio.test.ts
@@ -61,10 +61,25 @@ describe('OkuRadioGroup', () => {
     const value3 = wrapper.find('[value="3"]')
     await value3.trigger('click')
     expect(wrapper.element).toMatchSnapshot()
-    expect(value3.classes()).toContain('checked')
+    expect(value3.attributes('aria-checked')).toBe('true')
+    expect(value2.attributes('aria-checked')).toBe('false')
+
+    const span = value3.find('span')
+    expect(span.classes()).toContain('indicator-class')
+    expect(span.attributes('data-state')).toBe('checked')
+
+    expect(value2.find('span').exists()).toBe(false)
 
     const value1 = wrapper.find('[value="1"]')
     await value1.trigger('click')
     expect(wrapper.element).toMatchSnapshot()
+
+    expect(value1.attributes('aria-checked')).toBe('true')
+    expect(value2.attributes('aria-checked')).toBe('false')
+    expect(value3.attributes('aria-checked')).toBe('false')
+
+    expect(value1.find('span').exists()).toBe(true)
+    expect(value2.find('span').exists()).toBe(false)
+    expect(value3.find('span').exists()).toBe(false)
   })
 })
