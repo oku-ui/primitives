55ba6d3db97cfe40258695f4fb30006551023762
productdevbook <hi@productdevbook.com>
2023-08-21 13:18:13 +0300

    fix: type

--- packages/components/dismissable-layer/src/index.ts
+++ packages/components/dismissable-layer/src/index.ts
@@ -3,6 +3,5 @@ export * from './DismissableLayerBranch'
 export { OkuDismissableLayer } from './DismissableLayer'
 
 export type {
-  InstanceDismissableLayerType,
   DismissableLayerProps,
 } from './DismissableLayer'
