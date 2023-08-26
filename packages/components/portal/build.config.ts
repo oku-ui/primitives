74ba5194b1b4d75ec533a1c12a3ee9353aa56b16
productdevbook <hi@productdevbook.com>
2023-08-26 15:50:54 +0300

    feat: unbuild system

--- packages/components/portal/build.config.ts
+++ packages/components/portal/build.config.ts
@@ -1,12 +1,7 @@
 import { defineBuildConfig } from 'unbuild'
 
+const isClean = (process.env.CLEAN || 'false') === 'true'
 export default defineBuildConfig({
-  entries: [
-    {
-      builder: 'mkdist',
-      input: './src/',
-      pattern: ['**/!(*.test|*.stories).ts'],
-    },
-  ],
   declaration: true,
+  clean: isClean,
 })
