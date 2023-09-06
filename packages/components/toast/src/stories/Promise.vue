6d043aa40a183b19cc86e9f58975af7d5a856340
Cr0zy07 <49247629+Cr0zy07@users.noreply.github.com>
2023-09-06 14:14:51 +0800

    feat(toast): new component (#279)
    * feat: toast component
    
    * chore: update package
    
    * chore: update package
    
    * chore: `Oku` typo add component name
    
    * chore: refactor `context` name
    
    * fix: toast-provider
    
    * chore: fix toast-viewport
    
    * chore: fix toast-focus-proxy
    
    * chore: update toast
    
    * chore: update toast-impl
    
    * fix: lint issues
    
    * chore: update toast-announce
    
    * chore: update toast-title
    
    * chore: update toast-description
    
    * chore: update toast close
    
    * chore: update toast-action
    
    * fix: children slots
    
    * chore: fix type
    
    * fix: add package packages
    
    * chore: fix test
    
    * chore: add stories
    
    * fix: build
    
    * fix: typo
    
    * chore: update
    
    * chore: fix merge
    
    * chore: update
    
    * chore: update
    
    * chore: fix
    
    * refactor: formatting
    
    * fix: typo
    
    * fix: styling
    
    * chore: fix
    
    * refactor: revert a change and formatting
    
    * fix: toast initial value
    
    * fix: stories style props and emits
    
    * chore: fix
    
    * fix: teleport and formatting
    
    * refactor: add Chromatic story
    
    * refactor: split into separate files
    
    * chore: after merge fix errors
    
    * fix: `Styled` and `Chromatic`
    
    * fix: empty el
    
    * chore: delete empty file

---
+++ packages/components/toast/src/stories/Promise.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { OkuToast, OkuToastDescription, OkuToastProvider, OkuToastViewport } from '@oku-ui/toast'
+import { ref } from 'vue'
+
+const open = ref(false)
+const saving = ref(false)
+</script>
+
+<template>
+  <OkuToastProvider>
+    <form
+      @submit.prevent="() => {
+        saving = true;
+        open = true;
+      }"
+    >
+      <button type="submit" class="px-4 py-2 bg-gray-200 hover:bg-gray-300 text-gray-800 text-sm font-medium rounded-md">
+        Save
+      </button>
+      <OkuToast v-model="open" class="toast" :duration="saving ? Infinity : 2000">
+        <OkuToastDescription v-if="saving">
+          Saving&hellip;
+        </OkuToastDescription>
+        <OkuToastDescription v-else>
+          Saved!
+        </OkuToastDescription>
+      </OkuToast>
+    </form>
+
+    <OkuToastViewport class="viewport" />
+  </OkuToastProvider>
+</template>
+
+<style lang="postcss">
+@import './toast.css'
+</style>
---
+++ packages/components/toast/src/stories/ToastSubscribeSuccess.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { OkuToast, OkuToastClose, OkuToastDescription, OkuToastTitle } from '@oku-ui/toast'
+
+defineProps({
+  open: Boolean,
+})
+</script>
+
+<template>
+  <OkuToast class="toast" :open="open">
+    <div class="header success-header">
+      <OkuToastTitle class="title">
+        Success!
+      </OkuToastTitle>
+      <OkuToastClose class="button close" aria-label="Close">
+        <span aria-hidden>×</span>
+      </OkuToastClose>
+    </div>
+    <OkuToastDescription class="description">
+      You have subscribed. We'll be in touch.
+    </OkuToastDescription>
+  </OkuToast>
+</template>
+
+<style scoped lang="postcss">
+@import './toast.css'
+.success-header {
+  background: green;
+}
+</style>
