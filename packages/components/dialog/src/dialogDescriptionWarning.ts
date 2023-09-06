1547e7ca65cef7d4cb559d6bdaf147feec99b44c
productdevbook <hi@productdevbook.com>
2023-09-06 15:36:34 +0300

    chore: update

--- packages/components/dialog/src/dialogDescriptionWarning.ts
+++ packages/components/dialog/src/dialogDescriptionWarning.ts
@@ -1,19 +1,19 @@
-import type { PropType, Ref } from 'vue'
-import { defineComponent, watchEffect } from 'vue'
+import type { PropType } from 'vue'
+import { defineComponent, toRefs, watchEffect } from 'vue'
 import type { DialogContentNaviteElement } from './dialogContent'
 import { useWarningInject } from './utils'
 
 export const DESCRIPTION_WARNING_NAME = 'OkuDialogDescriptionWarning'
 
 export type DescriptionWarningProps = {
-  contentRef: Ref<DialogContentNaviteElement>
+  contentRef: DialogContentNaviteElement
   descriptionId?: string
 }
 
 export const dialogDescriptionWarningProps = {
   props: {
     contentRef: {
-      type: [HTMLDivElement] as PropType<HTMLDivElement>,
+      type: Object as PropType<HTMLElement | null>,
       required: true,
     },
     descriptionId: {
@@ -32,23 +32,25 @@ const dialogDescriptionWarning = defineComponent({
     ...dialogDescriptionWarningProps.props,
   },
   setup(props) {
+    const { contentRef, descriptionId } = toRefs(props)
+
     const inject = useWarningInject(DESCRIPTION_WARNING_NAME)
 
     const MESSAGE = `Warning: Missing \`Description\` or \`aria-describedby={undefined}\` for {${(inject as any).contentName}}.`
 
     watchEffect(() => {
-      const describedById = props.contentRef?.getAttribute('aria-describedby')
+      const describedById = contentRef.value?.getAttribute('aria-describedby')
       // if we have an id and the user hasn't set aria-describedby={undefined}
-      if (props.descriptionId && describedById) {
-        const hasDescription = document.getElementById(props.descriptionId)
+      if (descriptionId.value && describedById) {
+        const hasDescription = document.getElementById(descriptionId.value)
         if (!hasDescription)
           console.warn(MESSAGE)
       }
     })
 
-    return null
+    return () => null
   },
 })
 
 // TODO: https://github.com/vuejs/core/pull/7444 after delete
-export const OkuDialogDescriptionWarning = dialogDescriptionWarning as typeof dialogDescriptionWarning
+export const OkuDialogDescriptionWarning = dialogDescriptionWarning
