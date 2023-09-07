1d7ea6712ecdb147a8c97b5b0ed2a07d7c313e73
Shehab Rahal <shehabrahal1@gmail.com>
2023-09-07 21:24:56 +0800

    chore: update scroll-area-corner-impl

--- packages/components/scroll-area/src/scroll-area-corner-impl.ts
+++ packages/components/scroll-area/src/scroll-area-corner-impl.ts
@@ -1,71 +1,72 @@
 import { Primitive, primitiveProps } from '@Oku-ui/primitive'
-import type { ElementType, PrimitiveProps } from '@Oku-ui/primitive'
+import type { OkuElement, PrimitiveProps } from '@Oku-ui/primitive'
 import { defineComponent, h, ref } from 'vue'
-import { useForwardRef } from '../../../core/use-composable/dist'
-import { scopedProps } from './types'
+import { useForwardRef } from '@oku-ui/use-composable'
+import { scopedScrollAreaProps } from './types'
 import { CORNER_NAME } from './scroll-area-corner'
-import { useScrollAreaContext } from './scroll-area'
+import { useScrollAreaInject } from './scroll-area'
+import { useResizeObserver } from './utils'
 
-export type ScrollAreaCornerImplIntrinsicElement = ElementType<'div'>
-type ScrollAreaCornerImplElement = HTMLDivElement
+export type ScrollAreaCornerImplNaviteElement = OkuElement<'div'>
+export type ScrollAreaCornerImplElement = HTMLDivElement
 
-interface ScrollAreaCornerImplProps extends PrimitiveProps { }
+export interface ScrollAreaCornerImplProps extends PrimitiveProps { }
+
+const scrollAreaCornerImplProps = {
+  props: {},
+  emits: {},
+}
 
 const scrollAreaCornerImpl = defineComponent({
-  // name: SCROLL_NAME,
+  name: CORNER_NAME,
   inheritAttrs: false,
   props: {
-    ...scopedProps,
+    ...scrollAreaCornerImplProps.props,
+    ...scopedScrollAreaProps,
     ...primitiveProps,
   },
-  setup(props, { attrs }) {
-    const { ...scrollAreaCornerImplAttrs } = attrs as ScrollAreaCornerImplIntrinsicElement
+  emits: scrollAreaCornerImplProps.emits,
+  setup(props, { attrs, slots }) {
+    // const { ...scrollAreaCornerImplAttrs } = attrs as ScrollAreaCornerImplNaviteElement
 
     const forwardedRef = useForwardRef()
 
-    const context = useScrollAreaContext(CORNER_NAME, props.scopeOkuScrollArea)
+    const inject = useScrollAreaInject(CORNER_NAME, props.scopeOkuScrollArea)
     const width = ref<number>(0)
     const height = ref<number>(0)
     const hasSize = Boolean(width.value && height)
 
-    useResizeObserver(context.scrollbarX, () => {
-      const height = context.scrollbarX?.offsetHeight || 0
-      context.onCornerHeightChange(height)
-      setHeight(height)
-      height.value = height
+    useResizeObserver(inject.scrollbarX, () => {
+      const _height = inject.scrollbarX?.offsetHeight || 0
+      inject.onCornerHeightChange(_height)
+      height.value = _height
     })
 
-    useResizeObserver(context.scrollbarY, () => {
-      const width = context.scrollbarY?.offsetWidth || 0
-      context.onCornerWidthChange(width)
-      width.value = width
+    useResizeObserver(inject.scrollbarY, () => {
+      const _width = inject.scrollbarY?.offsetWidth || 0
+      inject.onCornerWidthChange(_width)
+      width.value = _width
     })
 
-    const originalReturn = () =>
-      hasSize
-        ? h(
-          Primitive.div,
-          {
-            ...scrollAreaCornerImplAttrs,
-            ref: forwardedRef,
-            style: {
-              width,
-              height,
-              position: 'absolute',
-              right: context.dir === 'ltr' ? 0 : undefined,
-              left: context.dir === 'rtl' ? 0 : undefined,
-              bottom: 0,
-            },
-            ...props.scopeOkuScrollArea.style,
+    return () => hasSize
+      ? h(Primitive.div,
+        {
+          ...attrs,
+          ref: forwardedRef,
+          style: {
+            width: width.value,
+            height: height.value,
+            position: 'absolute',
+            right: inject.dir === 'ltr' ? 0 : undefined,
+            left: inject.dir === 'rtl' ? 0 : undefined,
+            bottom: 0,
+            ...attrs.style as CSSStyleRule,
           },
-        )
-        : null
-
-    return originalReturn
+        }, slots,
+      )
+      : null
   },
 })
 
 export const OkuScrollAreaCornerImpl = scrollAreaCornerImpl as typeof scrollAreaCornerImpl &
 (new () => { $props: Partial<ScrollAreaCornerImplElement> })
-
-export type { ScrollAreaCornerImplElement, ScrollAreaCornerImplProps }
