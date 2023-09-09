05a3a1adc5464016405b3137802e424b32ee9d3a
Shehab Rahal <shehabrahal1@gmail.com>
2023-09-09 10:57:17 +0800

    chore: update package

---
+++ packages/components/scroll-area/src/index.ts
@@ -0,0 +1,94 @@
+export { OkuScrollArea } from './scroll-area'
+export { OkuScrollAreaCorner } from './scroll-area-corner'
+export { OkuScrollAreaCornerImpl } from './scroll-area-corner-impl'
+export { OkuScrollAreaScrollbar } from './scroll-area-scrollbar'
+export { OkuScrollAreaScrollbarAuto } from './scroll-area-scrollbar-auto'
+export { OkuScrollAreaScrollbarX } from './scroll-area-scrollbar-axis-x'
+export { OkuScrollAreaScrollbarY } from './scroll-area-scrollbar-axis-y'
+export { OkuScrollAreaScrollbarHover } from './scroll-area-scrollbar-hover'
+export { OkuScrollAreaScrollbarImpl } from './scroll-area-scrollbar-impl'
+export { OkuScrollAreaScrollbarScroll } from './scroll-area-scrollbar-scroll'
+export { OkuScrollAreaScrollbarVisible } from './scroll-area-scrollbar-visible'
+export { OkuScrollAreaThumb } from './scroll-area-thumb'
+export { OkuScrollAreaThumbImpl } from './scroll-area-thumb-impl'
+export { OkuScrollAreaViewport } from './scroll-area-viewport'
+
+export type {
+  ScrollAreaProps,
+  ScrollAreaElement,
+  ScrollAreaNaviteElement,
+} from './scroll-area'
+
+export type {
+  ScrollAreaCornerProps,
+  ScrollAreaCornerElement,
+  ScrollAreaCornerNaviteElement,
+} from './scroll-area-corner'
+
+export type {
+  ScrollAreaCornerImplProps,
+  ScrollAreaCornerImplElement,
+  ScrollAreaCornerImplNaviteElement,
+} from './scroll-area-corner-impl'
+
+export type {
+  ScrollAreaScrollbarProps,
+  ScrollAreaScrollbarElement,
+  ScrollAreaScrollbarNaviteElement,
+} from './scroll-area-scrollbar'
+
+export type {
+  ScrollAreaScrollbarAutoProps,
+  ScrollAreaScrollbarAutoElement,
+  ScrollAreaScrollbarAutoNaviteElement,
+} from './scroll-area-scrollbar-auto'
+
+// export type {
+// } from './scroll-area-scrollbar-axis-x'
+
+// export type {
+// } from './scroll-area-scrollbar-axis-y'
+
+export type {
+  ScrollAreaScrollbarHoverProps,
+  ScrollAreaScrollbarHoverElement,
+  ScrollAreaScrollbarHoverNaviteElement,
+} from './scroll-area-scrollbar-hover'
+
+export type {
+  ScrollAreaScrollbarImplProps,
+  ScrollAreaScrollbarImplEmits,
+  ScrollAreaScrollbarImplElement,
+  ScrollAreaScrollbarImplNaviteElement,
+} from './scroll-area-scrollbar-impl'
+
+export type {
+  ScrollAreaScrollbarScrollProps,
+  ScrollAreaScrollbarScrollElement,
+  ScrollAreaScrollbarScrollNaviteElement,
+} from './scroll-area-scrollbar-scroll'
+
+export type {
+  ScrollAreaScrollbarVisibleProps,
+  ScrollAreaScrollbarVisibleElement,
+  ScrollAreaScrollbarVisibleNaviteElement,
+} from './scroll-area-scrollbar-visible'
+
+export type {
+  ScrollAreaThumbProps,
+  ScrollAreaThumbElement,
+  ScrollAreaThumbNaviteElement,
+} from './scroll-area-thumb'
+
+export type {
+  ScrollAreaThumbImplProps,
+  scrollAreaThumbImplEmits,
+  ScrollAreaThumbImplElement,
+  ScrollAreaThumbImplNaviteElement,
+} from './scroll-area-thumb-impl'
+
+export type {
+  ScrollAreaViewportProps,
+  ScrollAreaViewportElement,
+  ScrollAreaViewportNaviteElement,
+} from './scroll-area-viewport'
---
+++ packages/components/scroll-area/src/scroll-area.test.ts
@@ -0,0 +1,7 @@
+import { describe, expect, it } from 'vitest'
+
+describe('OkuScrollArea', () => {
+  it('should be ok', () => {
+    expect(1).toBe(1)
+  })
+})
