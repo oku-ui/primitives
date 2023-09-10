e02c7e8c7d5b024deba7251b8d42f509e3bb10a8
Mustafa Türköz <mturkoz93@gmail.com>
2023-09-10 17:59:59 +0300

    feat(hover-card): new component (#334)
    * feat(hovercard): new component
    
    * fix: updated hovercard
    
    * fix: fixed hovercard components
    
    * fix: update code
    
    * chore: refactor
    
    * chore: fix lint
    
    * fix: code
    
    * fix: lint issues
    
    * chore: story add
    
    * chore: fix ref
    
    * chore: add new stories
    
    * fix: typecheck
    
    * chore: added stories for hovercard
    
    * chore: refactor
    
    * fix: lock
    
    * chore: fix
    
    * chore: update read me
    
    ---------
    
    Co-authored-by: productdevbook <hi@productdevbook.com>

---
+++ packages/components/hover-card/src/stories/Animated.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import {
+  OkuHoverCard,
+  OkuHoverCardArrow,
+  OkuHoverCardContent,
+  OkuHoverCardPortal,
+  OkuHoverCardTrigger,
+} from '@oku-ui/hover-card'
+import CardContentPlaceholder from './CardContentPlaceholder.vue'
+</script>
+
+<template>
+  <div style="padding: 50px; display: flex; justify-content: center">
+    <OkuHoverCard>
+      <OkuHoverCardTrigger href="/" class="triggerClass">
+        trigger
+      </OkuHoverCardTrigger>
+      <OkuHoverCardPortal>
+        <OkuHoverCardContent class="animatedContentClass" :side-offset="10">
+          <OkuHoverCardArrow class="arrowClass" :width="20" :height="10" />
+          <CardContentPlaceholder />
+        </OkuHoverCardContent>
+      </OkuHoverCardPortal>
+    </OkuHoverCard>
+  </div>
+</template>
---
+++ packages/components/hover-card/src/stories/AsyncUpdate.vue
@@ -0,0 +1,55 @@
+<script setup lang="ts">
+import {
+  OkuHoverCard,
+  OkuHoverCardArrow,
+  OkuHoverCardContent,
+  OkuHoverCardPortal,
+  OkuHoverCardTrigger,
+} from '@oku-ui/hover-card'
+import { onBeforeUnmount, ref } from 'vue'
+import CardContentPlaceholder from './CardContentPlaceholder.vue'
+
+const open = ref(false)
+const contentLoaded = ref(false)
+const timerRef = ref(0)
+
+function handleOpenChange(newOpen: any) {
+  clearTimeout(timerRef.value)
+
+  if (newOpen) {
+    timerRef.value = window.setTimeout(() => {
+      contentLoaded.value = true
+    }, 500)
+  }
+  else {
+    contentLoaded.value = false
+  }
+
+  open.value = newOpen
+}
+
+onBeforeUnmount(() => {
+  clearTimeout(timerRef.value)
+})
+</script>
+
+<template>
+  <div style="padding: 50px; display: flex; justify-content: center">
+    <OkuHoverCard v-model="open" @open-change="handleOpenChange(!open)">
+      <OkuHoverCardTrigger href="/" class="triggerClass">
+        trigger
+      </OkuHoverCardTrigger>
+      <OkuHoverCardPortal>
+        <OkuHoverCardContent class="contentClass" :side-offset="5">
+          <OkuHoverCardArrow class="arrowClass" :width="20" :height="10" />
+          <template v-if="contentLoaded">
+            <CardContentPlaceholder />
+          </template>
+          <template v-else>
+            Loading...
+          </template>
+        </OkuHoverCardContent>
+      </OkuHoverCardPortal>
+    </OkuHoverCard>
+  </div>
+</template>
