e7815d34201ef270e76a7e84f7d56b6cdedea8b4
productdevbook <hi@productdevbook.com>
2023-09-10 09:35:48 +0300

    fix: progress

--- packages/components/portal/src/Portal.ts
+++ packages/components/portal/src/Portal.ts
@@ -6,7 +6,7 @@ import type {
 } from '@oku-ui/primitive'
 import { useForwardRef } from '@oku-ui/use-composable'
 import type { PropType } from 'vue'
-import { Teleport, defineComponent, h, toRefs } from 'vue'
+import { Teleport, defineComponent, h, mergeProps, reactive, toRefs } from 'vue'
 
 const PORTAL_NAME = 'OkuPortal'
 
@@ -39,19 +39,21 @@ const portal = defineComponent({
     ...primitiveProps,
   },
   setup(props, { slots, attrs }) {
-    const { asChild, container } = toRefs(props)
+    const { container, ...portalProps } = toRefs(props)
+    const reactivePortalProps = reactive(portalProps)
 
     const forwardedRef = useForwardRef()
 
     return () => container.value
-      ? h(Teleport, { to: container.value, disabled: false }, h(Primitive.div,
-        {
-          ...attrs,
-          ref: forwardedRef,
-          asChild: asChild.value,
-        }, {
-          default: () => slots.default?.(),
-        }),
+      ? h(Teleport,
+        { to: container.value, disabled: false },
+        h(Primitive.div,
+          {
+            ...mergeProps(attrs, reactivePortalProps),
+            ref: forwardedRef,
+          }, {
+            default: () => slots.default?.(),
+          }),
       )
       : null
   },
