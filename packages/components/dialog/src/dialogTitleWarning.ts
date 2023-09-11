d786d9db4f821fd19eace5ced2a81f05ef937c45
Kubra KIRAS <kbr.aslan.k@gmail.com>
2023-09-11 15:06:46 +0300

    feat(dialog): new component (#338)
    * dialog added
    
    * chore: update
    
    * chore: update
    
    * chore: update
    
    * chore: update
    
    * chore: update
    
    * chore:update
    
    * chore:update
    
    * chore: fix error cli
    
    * chore: update
    
    * chore: fix pnpm workspace
    
    * chore: update
    
    * chore:update
    
    * chore: update fix
    
    * chore:update
    
    * chore: update
    
    * chore:update
    
    * chore: lint
    
    * chore: fix autofix
    
    * chore: update pnpm lock
    
    * chore: add nuxt
    
    * chore: update readme
    
    * chore: refactor props
    
    * chore: update code
    
    * chore:update
    
    * chore:update
    
    * chore:update
    
    * chore:update
    
    * chore: fix pnpm
    
    * refactor: props

---
+++ packages/components/dialog/src/dialogTitleWarning.ts
@@ -0,0 +1,44 @@
+import { defineComponent, toRef, watchEffect } from 'vue'
+import { TITLE_WARNING_NAME, useWarningInject } from './utils'
+
+export type TitleWarningProps = { titleId?: string }
+
+export const dialogTitleWarningProps = {
+  props: {
+    titleId: {
+      type: String,
+    },
+  },
+  emits: {
+  },
+}
+const dialogTitleWarning = defineComponent({
+  name: TITLE_WARNING_NAME,
+  inheritAttrs: false,
+  props: {
+    ...dialogTitleWarningProps.props,
+  },
+  setup(props) {
+    const titleId = toRef(props, 'titleId')
+    const inject = useWarningInject(TITLE_WARNING_NAME)
+
+    const MESSAGE = `${inject?.contentName.value}\` requires a \`${inject.titleName.value}\` for the component to be accessible for screen reader users.
+
+    If you want to hide the \`${inject?.titleName.value}\`, you can wrap it with our VisuallyHidden component.
+    
+    For more information, see https://oku-ui.com/primitives/components/${inject?.docsSlug.value}`
+
+    watchEffect(() => {
+      if (titleId.value) {
+        const hasTitle = document.getElementById(titleId.value)
+        if (!hasTitle)
+          throw new Error(MESSAGE)
+      }
+    })
+
+    return () => null
+  },
+})
+
+// TODO: https://github.com/vuejs/core/pull/7444 after delete
+export const OkuDialogTitleWarning = dialogTitleWarning as typeof dialogTitleWarning
---
+++ packages/components/dialog/src/stories/CustomFocus.vue
@@ -0,0 +1,76 @@
+<script setup lang="ts">
+import { OkuDialog, OkuDialogClose, OkuDialogContent, OkuDialogDescription, OkuDialogOverlay, OkuDialogPortal, OkuDialogTitle, OkuDialogTrigger } from '@oku-ui/dialog'
+import { ref } from 'vue'
+
+const open = ref(false)
+
+const firstNameRef = ref<HTMLInputElement | null>(null)
+const searchFieldRef = ref<HTMLInputElement | null>
+(null)
+
+function onOpenAutoFocus(event: any) {
+  console.log('onOpenAutoFocus')
+  event.preventDefault()
+
+  firstNameRef.value?.focus()
+}
+function onCloseAutoFocus(event: any) {
+  console.log('onCloseAutoFocus')
+  event.preventDefault()
+  searchFieldRef.value?.focus()
+}
+</script>
+
+<template>
+  <div>
+    <OkuDialog v-model="open">
+      <OkuDialogTrigger class="triggerClass">
+        open
+      </OkuDialogTrigger>
+      <OkuDialogPortal>
+        <OkuDialogOverlay class="overlayClass" />
+        <OkuDialogContent
+          class="contentDefaultClass"
+          @open-auto-focus="onOpenAutoFocus"
+          @close-auto-focus="onCloseAutoFocus"
+        >
+          <OkuDialogClose>
+            close
+          </OkuDialogClose>
+          <OkuDialogTitle>
+            Title
+          </OkuDialogTitle>
+          <OkuDialogDescription>
+            The first name input will receive the focus after opening the dialog.
+          </OkuDialogDescription>
+          <table style="margin-top: 20px;">
+            <tr>
+              <td>
+                <label htmlFor="firstName">First Name</label>
+                <input ref="firstNameRef" type="text" placeholder="John">
+              </td>
+              <td>
+                <label htmlFor="lastName">Last Name</label>
+                <input id="lastName" type="text" placeholder="Doe">
+              </td>
+              <td>
+                <button type="submit">
+                  Send
+                </button>
+              </td>
+            </tr>
+          </table>
+        </OkuDialogContent>
+      </OkuDialogPortal>
+    </okudialog>
+    <div style="margin-top: 20px;">
+      <p>These elements can't be focused when the dialog is opened.</p>
+      <div style="margin-top: 15px;">
+        <button type="button">
+          A button
+        </button>
+        <input ref="searchFieldRef" type="text" placeholder="Another focusable element">
+      </div>
+    </div>
+  </div>
+</template>
