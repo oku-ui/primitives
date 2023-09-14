216a8e8fafa1c2e123411131d0e7285a2f36b90a
Mehmet <hi@productdevbook.com>
2023-09-14 15:10:08 +0300

    refactor: delete storybook tailwindcss (#359)

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,9 @@ const { iconsPlugin, getIconCollections } = require('@egoist/tailwindcss-icons')
 export default {
   darkMode: 'class',
   content: [
-    './stories/**/*.stories.mdx',
-    './stories/**/*.stories.@(js|jsx|ts|tsx)',
-    './src/runtime/components/html/**/*.vue',
-    './src/runtime/components/**/*.vue',
-    './packages/**/*.vue',
+    // './src/runtime/components/html/**/*.vue',
+    // './src/runtime/components/**/*.vue',
+    // './packages/**/*.vue',
   ],
   theme: {
     extend: {},
