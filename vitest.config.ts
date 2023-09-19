8e3a468b9a7335b4675cdca3b229cb226fd726e9
productdevbook <hi@productdevbook.com>
2023-09-19 06:56:59 +0300

    fix: pnpm lock

--- vitest.config.ts
+++ vitest.config.ts
@@ -19,6 +19,7 @@ export default defineConfig({
     globals: true,
     alias: {
       '@oku-ui/checkbox': 'packages/components/checkbox/src',
+      '@oku-ui/alert-dialog': 'packages/components/alert-dialog/src',
       '@oku-ui/portal': 'packages/components/portal/src',
       '@oku-ui/slider': 'packages/components/slider/src',
       '@oku-ui/label': 'packages/components/label/src',
