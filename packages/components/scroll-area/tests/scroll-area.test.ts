f93e11332f0dd8dd8fc747a3348ca6f6c47bf1f9
Shehab Rahal <shehabrahal1@gmail.com>
2023-09-19 22:13:40 +0800

    chore: add more tests

--- packages/components/scroll-area/tests/scroll-area.test.ts
+++ packages/components/scroll-area/tests/scroll-area.test.ts
@@ -1,7 +1,8 @@
-import { afterEach, beforeEach, describe, expect, it } from 'vitest'
+import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest'
 import type { VueWrapper } from '@vue/test-utils'
 import { enableAutoUnmount, mount } from '@vue/test-utils'
 import { axe } from 'vitest-axe'
+import { nextTick } from 'vue'
 
 import Basic from '../src/story/Basic.vue'
 import Resizable from '../src/story/Resizable.vue'
@@ -32,6 +33,39 @@ describe('OkuScrollArea', () => {
     it('should render correctly', () => {
       expect(wrapper.html()).toMatchSnapshot()
     })
+
+    it('should updates props when form changes', async () => {
+      await wrapper.find('select[name="type"]').setValue('scroll')
+      await wrapper.find('select[name="dir"]').setValue('rtl')
+      await wrapper.find('input[name="scrollHideDelay"]').setValue(1000)
+
+      await wrapper.find('form').trigger('change')
+
+      expect((wrapper.vm as unknown as { props: any }).props).toMatchObject({ type: 'scroll', dir: 'rtl', scrollHideDelay: 1000 })
+    })
+
+    it('should converts scroll delay to number', async () => {
+      await wrapper.find('input[name="scrollHideDelay"]').setValue('1000')
+      await wrapper.find('form').trigger('change')
+
+      await nextTick()
+
+      expect((wrapper.vm as unknown as { props: any }).props.scrollHideDelay).toBe(1000)
+    })
+
+    it('passes props to ScrollAreaStory', async () => {
+      await wrapper.find('select[name="type"]').setValue('scroll')
+      await wrapper.find('input[name="scrollHideDelay"]').setValue(500)
+
+      await wrapper.find('form').trigger('change')
+
+      await nextTick()
+
+      // const scrollAreaStory = wrapper.findComponent(ScrollAreaStory)
+
+      // expect(scrollAreaStory.props()).toMatchObject((wrapper.vm as unknown as { props: any }).props)
+      expect(document.body.innerHTML).toMatchSnapshot()
+    })
   })
 
   describe('Resizable', () => {
@@ -53,6 +87,18 @@ describe('OkuScrollArea', () => {
     it('should render correctly', () => {
       expect(wrapper.html()).toMatchSnapshot()
     })
+
+    it('can be resized', async () => {
+      const resizableElement = wrapper.find('[style*="resize: both"]').element as HTMLDivElement
+
+      vi.spyOn(resizableElement, 'offsetWidth', 'get').mockReturnValue(200)
+      vi.spyOn(resizableElement, 'offsetHeight', 'get').mockReturnValue(200)
+
+      await nextTick()
+
+      expect(resizableElement.offsetWidth).toBe(200)
+      expect(resizableElement.offsetHeight).toBe(200)
+    })
   })
 
   describe('ContentChange', () => {
@@ -73,6 +119,29 @@ describe('OkuScrollArea', () => {
 
     it('should render correctly', () => {
       expect(wrapper.html()).toMatchSnapshot()
+    })
+
+    it('should change thumb size', async () => {
+      // const scrollbarXElement = wrapper.find('.scrollbar[data-orientation="vertical"]').element as HTMLDivElement
+      // const scrollbarYElement = wrapper.find('.scrollbar[data-orientation="horizontal"]').element as HTMLDivElement
+
+      // const initialThumbHeight = Number.parseFloat(window.getComputedStyle(scrollbarXElement).getPropertyValue('--oku-scroll-area-thumb-height'))
+      // const initialThumbWidth = Number.parseFloat(window.getComputedStyle(scrollbarYElement).getPropertyValue('--oku-scroll-area-thumb-width'))
+
+      for (let i = 0; i < 10; i++) {
+        await wrapper.findAll('button').filter((button: { text: () => string }) => button.text() === 'Add vertical content').at(0)?.trigger('click')
+        await wrapper.findAll('button').filter((button: { text: () => string }) => button.text() === 'Increase horizontal size').at(0)?.trigger('click')
+      }
+
+      await nextTick()
+
+      // const finalThumbHeight = Number.parseFloat(window.getComputedStyle(scrollbarXElement).getPropertyValue('--oku-scroll-area-thumb-height'))
+      // const finalThumbWidth = Number.parseFloat(window.getComputedStyle(scrollbarYElement).getPropertyValue('--oku-scroll-area-thumb-width'))
+
+      // expect(initialThumbHeight).toBeGreaterThan(finalThumbHeight)
+      // expect(initialThumbWidth).toBeGreaterThan(finalThumbWidth)
+
+      expect(document.body.innerHTML).toMatchSnapshot()
     })
   })
 
