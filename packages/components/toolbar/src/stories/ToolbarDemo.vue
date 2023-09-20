8cbf0469d217ff6ea58318ef2ecc60f1eb5dd68e
productdevbook <hi@productdevbook.com>
2023-09-20 06:11:21 +0300

    chore: toolbar

--- packages/components/toolbar/src/stories/ToolbarDemo.vue
+++ packages/components/toolbar/src/stories/ToolbarDemo.vue
@@ -22,3 +22,159 @@ withDefaults(defineProps<OkuToolbarProps>(), {
     </div>
   </div>
 </template>
+
+<style>
+.toolbar_rootClass {
+  @apply flex gap-1;
+
+  &[data-orientation="vertical"] {
+    flex-direction: column;
+  }
+
+  display: inline-flex;
+  box-sizing: border-box;
+  min-width: 130px;
+  background-color: #fff;
+  border: 1px solid #f0f0f0;
+  border-radius: 6px;
+  padding: 5px;
+  box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.1);
+  font-family: apple-system,
+  BlinkMacSystemFont,
+  helvetica,
+  arial,
+  sans-serif;
+  font-size: 13px;
+
+  &:focus-within {
+    border-color: #000;
+  }
+}
+
+.toolbar_separatorClass {
+  height: 1px;
+  margin: 5px;
+  background-color: #f0f0f0;
+
+  &[data-orientation="vertical"] {
+    width: 1px;
+    height: auto;
+  }
+}
+
+.toolbar_itemClass {
+  border: 1px solid #000;
+  border-radius: 6px;
+  background-color: transparent;
+  padding: 5px 10px;
+  font-family: apple-system, BlinkMacSystemFont, helvetica, arial, sans-serif;
+  font-size: 13px;
+
+  display: flex;
+  align-items: center;
+
+  &:focus,
+  &:focus-within {
+    outline: none;
+    box-shadow: 0 0 0 2px rgba(0, 0, 0, 0.5);
+  }
+
+  &[data-disabled] {
+    opacity: 0.5;
+    user-select: none;
+  }
+}
+
+.toolbar_linkClass {
+  border: 1px solid #000;
+  border-radius: 6px;
+  background-color: transparent;
+  padding: 5px 10px;
+  font-family: apple-system, BlinkMacSystemFont, helvetica, arial, sans-serif;
+  font-size: 13px;
+
+  display: flex;
+  align-items: center;
+
+  &:focus,
+  &:focus-within {
+    outline: none;
+    box-shadow: 0 0 0 2px rgba(0, 0, 0, 0.5);
+  }
+
+  &[data-disabled] {
+    opacity: 0.5;
+    user-select: none;
+  }
+
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  color: black;
+}
+
+.toolbar_toggleButtonClass {
+  border: 1px solid #000;
+  border-radius: 6px;
+  background-color: transparent;
+  padding: 5px 10px;
+  font-family: apple-system, BlinkMacSystemFont, helvetica, arial, sans-serif;
+  font-size: 13px;
+
+  display: flex;
+  align-items: center;
+
+  &:focus,
+  &:focus-within {
+    outline: none;
+    box-shadow: 0 0 0 2px rgba(0, 0, 0, 0.5);
+  }
+
+  &[data-disabled] {
+    opacity: 0.5;
+    user-select: none;
+  }
+
+  &[data-state="on"] {
+    background: black;
+    color: white;
+  }
+}
+
+.toolbar_toggleGroupClass {
+  display: flex;
+  gap: 5px;
+
+  &[data-orientation="vertical"] {
+    flex-direction: column;
+  }
+}
+
+.toolbar_toggleItemClass {
+  border: 1px solid #000;
+  border-radius: 6px;
+  background-color: transparent;
+  padding: 5px 10px;
+  font-family: apple-system, BlinkMacSystemFont, helvetica, arial, sans-serif;
+  font-size: 13px;
+
+  display: flex;
+  align-items: center;
+
+  &:focus,
+  &:focus-within {
+    outline: none;
+    box-shadow: 0 0 0 2px rgba(0, 0, 0, 0.5);
+  }
+
+  &[data-disabled] {
+    opacity: 0.5;
+    user-select: none;
+  }
+
+  &[data-state="on"] {
+    background: black;
+    color: white;
+  }
+}
+</style>
