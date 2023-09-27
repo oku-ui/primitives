c943170fb49d255994fb69597f69d5277fef7683
productdevbook <hi@productdevbook.com>
2023-09-27 09:03:03 +0300

    fix: nuxt

--- playground/nuxt3/nuxt.config.ts
+++ playground/nuxt3/nuxt.config.ts
@@ -2,25 +2,26 @@ import { addComponent } from '@nuxt/kit'
 import { globbySync } from 'globby'
 import { resolve } from 'pathe'
 
-export const components = globbySync('../../packages/components/**/src/stories/*Demo.vue', {
-  ignore: ['**/node_modules/**', '**/dist/**', '**/node_modules/@oku-ui/**'],
-  deep: 4,
-})
-export const core = globbySync('../../packages/core/**/src/stories/*Demo.vue', {
-  ignore: ['**/node_modules/**', '**/dist/**', '**/node_modules/@oku-ui/**'],
-  deep: 4,
-})
-
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: [
     async () => {
+      const components = globbySync('../../packages/components/**/src/stories/*Demo.vue', {
+        ignore: ['**/node_modules/**', '**/dist/**', '**/node_modules/@oku-ui/**'],
+        deep: 4,
+      })
       if (components.length > 0) {
         components.forEach((component) => {
           // LabelDemo.vue -> LabelDemo
           addComponent({ filePath: resolve(component), name: component.split('/').pop()!.replace('.vue', ''), priority: 1 })
         })
       }
+
+      const core = globbySync('../../packages/core/**/src/stories/*Demo.vue', {
+        ignore: ['**/node_modules/**', '**/dist/**', '**/node_modules/@oku-ui/**'],
+        deep: 4,
+      })
+
       if (core.length > 0) {
         core.forEach((component) => {
           // LabelDemo.vue -> LabelDemo
