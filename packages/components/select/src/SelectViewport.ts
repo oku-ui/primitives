97bae83b96e64fcfd4cf1eeab797ef82ba97860a
productdevbook <hi@productdevbook.com>
2023-09-30 20:38:09 +0300

    fix: SelectViewport

--- packages/components/select/src/SelectViewport.ts
+++ packages/components/select/src/SelectViewport.ts
@@ -28,9 +28,7 @@ const SelectViewport = defineComponent({
     ...selectViewportProps.props,
     ...scopeSelectProps,
   },
-  emits: {
-    ...selectViewportProps.emits,
-  },
+  emits: selectViewportProps.emits,
   setup(props, { slots, emit, attrs }) {
     const { scopeOkuSelect, ...selectViewportProps } = toRefs(props)
 
@@ -84,53 +82,53 @@ const SelectViewport = defineComponent({
                   overflow: 'auto',
                   ...(attrs.style as CSSProperties),
                 },
-                'onScroll': composeEventHandlers((event: Event) => {
-                  emit('onscroll', event)
+                'onScroll': composeEventHandlers(
+                  event => emit('onscroll', event),
+                  (event: Event) => {
+                    const viewport = event.currentTarget as HTMLElement
 
-                  const viewport = event.currentTarget as HTMLElement
-
-                  const { contentWrapper, shouldExpandOnScrollRef }
+                    const { contentWrapper, shouldExpandOnScrollRef }
                     = viewportInject
 
-                  if (shouldExpandOnScrollRef?.value && contentWrapper?.value) {
-                    const scrolledBy = Math.abs(
-                      prevScrollTopRef.value - viewport.scrollTop!,
-                    )
+                    if (shouldExpandOnScrollRef?.value && contentWrapper?.value) {
+                      const scrolledBy = Math.abs(
+                        prevScrollTopRef.value - viewport.scrollTop!,
+                      )
 
-                    if (scrolledBy > 0) {
-                      const availableHeight
+                      if (scrolledBy > 0) {
+                        const availableHeight
                         = window.innerHeight - CONTENT_MARGIN * 2
-                      const cssMinHeight = Number.parseFloat(
-                        contentWrapper.value.style.minHeight,
-                      )
-                      const cssHeight = Number.parseFloat(
-                        contentWrapper.value.style.height,
-                      )
-                      const prevHeight = Math.max(cssMinHeight, cssHeight)
-
-                      if (prevHeight < availableHeight) {
-                        const nextHeight = prevHeight + scrolledBy
-                        const clampedNextHeight = Math.min(
-                          availableHeight,
-                          nextHeight,
+                        const cssMinHeight = Number.parseFloat(
+                          contentWrapper.value.style.minHeight,
                         )
+                        const cssHeight = Number.parseFloat(
+                          contentWrapper.value.style.height,
+                        )
+                        const prevHeight = Math.max(cssMinHeight, cssHeight)
 
-                        const heightDiff = nextHeight - clampedNextHeight
-                        contentWrapper.value.style.height
+                        if (prevHeight < availableHeight) {
+                          const nextHeight = prevHeight + scrolledBy
+                          const clampedNextHeight = Math.min(
+                            availableHeight,
+                            nextHeight,
+                          )
+
+                          const heightDiff = nextHeight - clampedNextHeight
+                          contentWrapper.value.style.height
                           = `${clampedNextHeight}px`
 
-                        if (contentWrapper.value.style.bottom === '0px') {
-                          viewport.scrollTop = heightDiff > 0 ? heightDiff : 0
-                          // ensure the content stays pinned to the bottom
-                          contentWrapper.value.style.justifyContent
+                          if (contentWrapper.value.style.bottom === '0px') {
+                            viewport.scrollTop = heightDiff > 0 ? heightDiff : 0
+                            // ensure the content stays pinned to the bottom
+                            contentWrapper.value.style.justifyContent
                             = 'flex-end'
+                          }
                         }
                       }
                     }
-                  }
 
-                  prevScrollTopRef.value = viewport?.scrollTop
-                }),
+                    prevScrollTopRef.value = viewport?.scrollTop
+                  }),
               },
               slots,
             ),
