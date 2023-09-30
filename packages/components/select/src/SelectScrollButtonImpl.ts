2ea6bfa7337d68d32fc6678775bd36830eb1bff1
productdevbook <hi@productdevbook.com>
2023-09-30 16:38:55 +0300

    fix: props

--- packages/components/select/src/SelectScrollButtonImpl.ts
+++ packages/components/select/src/SelectScrollButtonImpl.ts
@@ -12,7 +12,7 @@ import {
 import { Primitive } from '@oku-ui/primitive'
 import { useForwardRef } from '@oku-ui/use-composable'
 import { composeEventHandlers } from '@oku-ui/utils'
-import type { ItemData, SelectScrollButtonImplElement } from './types'
+import type { ItemData, SelectScrollButtonImplElement } from './props'
 import {
   SELECT_SCROLL_BUTTON,
   scopeSelectProps,
