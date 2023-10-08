70c0f64cfbaf94dfff184c4031fa93afebbe483b
productdevbook <hi@productdevbook.com>
2023-10-08 07:07:36 +0300

    fix: test

--- packages/components/menu/tests/menu.test.ts
+++ packages/components/menu/tests/menu.test.ts
@@ -1,7 +1,11 @@
-import { afterEach, describe } from 'vitest'
+import { afterEach, describe, expect } from 'vitest'
 import { enableAutoUnmount } from '@vue/test-utils'
+import { it } from 'node:test'
 
 enableAutoUnmount(afterEach)
 
 describe('OkuMenu', () => {
+  it('should be OkuMenu', () => {
+    expect(true).toBe(true)
+  })
 })
