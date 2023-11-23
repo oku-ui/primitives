e1b71c4152109ae56ab084c04bbc27ed65678eb4
Shehab Rahal <shehabrahal1@gmail.com>
2023-11-24 00:08:01 +0800

    refactor: aspect-ratio

--- packages/components/aspect-ratio/src/index.ts
+++ packages/components/aspect-ratio/src/index.ts
@@ -1,10 +1,9 @@
 export {
   OkuAspectRatio,
-  aspectRatioProps,
 } from './aspect-ratio'
 
 export type {
   AspectRatioProps,
   AspectRatioElement,
-  AspectRatioNaviteElement,
-} from './aspect-ratio'
+  AspectRatioNativeElement,
+} from './props'
