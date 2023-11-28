4edc9c4751666b93068c74cda6bc6dbe17ccf5dc
Shehab Rahal <shehabrahal1@gmail.com>
2023-11-28 19:17:22 +0800

    refactor: roving-focus use slot function

--- packages/core/roving-focus/src/RovingFocusGroup.ts
+++ packages/core/roving-focus/src/RovingFocusGroup.ts
@@ -22,27 +22,18 @@ const rovingFocusGroup = defineComponent({
   emits: rovingFocusGroupProps.emits,
   setup(props, { slots, attrs }) {
     const forwardedRef = useForwardRef()
-
     const emits = useListeners()
-    return () => {
-      return h(CollectionProvider, {
-        scope: props.scopeOkuRovingFocusGroup,
-      }, {
-        default: () => h(CollectionSlot, {
-          scope: props.scopeOkuRovingFocusGroup,
-        }, {
-          default: () => h(OkuRovingFocusGroupImpl, {
-            ...mergeProps(attrs, props, emits),
-            ref: forwardedRef,
-          }, slots.default?.()),
-        }),
-      })
-    }
+
+    return () => h(CollectionProvider, {
+      scope: props.scopeOkuRovingFocusGroup,
+    }, () => h(CollectionSlot, {
+      scope: props.scopeOkuRovingFocusGroup,
+    }, () => h(OkuRovingFocusGroupImpl, {
+      ...mergeProps(attrs, props, emits),
+      ref: forwardedRef,
+    }, () => slots.default?.())))
   },
 })
 
 // TODO: https://github.com/vuejs/core/pull/7444 after delete
-export const OkuRovingFocusGroup = rovingFocusGroup as typeof rovingFocusGroup &
-  (new () => {
-    $props: RovingFocusGroupNaviteElement
-  })
+export const OkuRovingFocusGroup = rovingFocusGroup as typeof rovingFocusGroup & (new () => { $props: RovingFocusGroupNaviteElement })
