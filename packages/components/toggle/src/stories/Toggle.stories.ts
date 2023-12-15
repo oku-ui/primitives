29afd1bce63ad5169360b5c930306627f7ecbd8b
Cr0zy07 <49247629+Cr0zy07@users.noreply.github.com>
2023-12-15 17:10:44 +0800

    refactor(toggle): update component (#452)
    * refactor: toggle
    
    * refactor: toggle-group change ToggleElementNaviteElement to ToggleElementNativeElement
    
    * refactor: add file path

---
+++ packages/components/toggle/src/stories/Toggle.stories.ts
@@ -0,0 +1,72 @@
+import type { Meta, StoryObj } from '@storybook/vue3'
+
+import type { IToggleProps } from './ToggleDemo.vue'
+import OkuToggle from './ToggleDemo.vue'
+
+interface StoryProps extends IToggleProps {
+}
+
+interface StoryProps extends IToggleProps { }
+
+const meta = {
+  title: 'Components/Toggle',
+  component: OkuToggle,
+  args: {
+    template: 'Styled',
+  },
+} satisfies Meta<typeof OkuToggle> & {
+  args: StoryProps
+}
+
+export default meta
+type Story = StoryObj<typeof meta> & {
+  args: StoryProps
+}
+
+export const Styled: Story = {
+  args: {
+    template: 'Styled',
+  },
+  render: (args: any) => ({
+    components: { OkuToggle },
+    setup() {
+      return { args }
+    },
+    template: `
+      <OkuToggle v-bind="args" />
+    `,
+  }),
+}
+
+export const Controlled: Story = {
+  args: {
+    template: 'Controlled',
+  },
+  render: (args: any) => ({
+    components: { OkuToggle },
+    setup() {
+      return { args }
+    },
+    template: `
+      <OkuToggle v-bind="args" />
+    `,
+  }),
+}
+
+export const Chromatic: Story = {
+  args: {
+    template: 'Chromatic',
+  },
+  render: (args: any) => ({
+    components: { OkuToggle },
+    setup() {
+      return { args }
+    },
+    template: `
+      <OkuToggle v-bind="args" />
+    `,
+  }),
+  parameters: {
+    chromatic: { disable: false },
+  },
+}
