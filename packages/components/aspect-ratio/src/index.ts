c400fec10dfd1111e35670dd16b8e1fe639ee9eb
Shehab Rahal <shehabrahal1@gmail.com>
2023-12-17 17:40:36 +0800

    refactor: remove unnecessary exports

--- packages/components/aspect-ratio/src/index.ts
+++ packages/components/aspect-ratio/src/index.ts
@@ -1,6 +1,4 @@
-export {
-  OkuAspectRatio,
-} from './aspect-ratio'
+export { OkuAspectRatio } from './aspect-ratio'
 
 export type {
   AspectRatioProps,
--- packages/components/toggle-group/src/index.ts
+++ packages/components/toggle-group/src/index.ts
@@ -1,8 +1,4 @@
 export { OkuToggleGroup } from './toggle-group'
-// export { OkuToggleGroupImpl } from './toggle-group-impl'
-// export { OkuToggleGroupImplSingle } from './toggle-group-impl-single'
-// export { OkuToggleGroupImplMultiple } from './toggle-group-impl-multiple'
-// export { OkuToggleGroupItemImpl } from './toggle-group-item-impl'
 export { OkuToggleGroupItem } from './toggle-group-item'
 
 export type {
@@ -11,33 +7,6 @@ export type {
   ToggleGroupElement,
   ToggleGroupNativeElement,
 } from './props'
-
-// export type {
-//   ToggleGroupImplProps,
-//   ToggleGroupImplElement,
-//   ToggleGroupImplNativeElement,
-// } from './props'
-
-// export type {
-//   ToggleGroupImplSingleProps,
-//   ToggleGroupImplSingleEmits,
-//   ToggleGroupImplSingleElement,
-//   ToggleGroupImplSingleNativeElement,
-// } from './props'
-
-// // export type {
-//   ToggleGroupImplMultipleProps,
-//   ToggleGroupImplMultipleEmits,
-//   ToggleGroupImplMultipleElement,
-//   ToggleGroupImplMultipleNativeElement,
-// } from './props'
-
-// export type {
-//   ToggleGroupItemImplProps,
-//   ToggleGroupItemImplEmits,
-//   ToggleGroupItemImplElement,
-//   ToggleGroupItemImplNativeElement,
-// } from './props'
 
 export type {
   ToggleGroupItemProps,
