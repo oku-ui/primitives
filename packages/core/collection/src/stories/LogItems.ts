abacacd54156cd6ad086f898c8efca2248769fd2
productdevbook <hi@productdevbook.com>
2024-01-06 07:27:00 +0300

    Remove unused collection files

--- packages/core/collection/src/stories/LogItems.ts
+++
@@ -1,7 +0,0 @@
-import { useCollection } from './utils'
-
-export function LogItems({ name = 'items' }: { name?: string }) {
-  const getItems = useCollection(undefined)
-
-  console.warn(name, getItems())
-}
