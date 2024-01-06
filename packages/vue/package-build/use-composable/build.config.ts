d847caa6e5a95f1b8b0f772574928ae33c62d2e5
productdevbook <hi@productdevbook.com>
2024-01-06 07:04:53 +0300

    Delete unused files and update package.json

---
+++ packages/vue/package-build/use-composable/build.config.ts
@@ -0,0 +1,12 @@
+import { defineBuildConfig } from 'unbuild'
+
+export default defineBuildConfig({
+  entries: [
+    { builder: 'mkdist', input: '../../src/packages/use-composable', pattern: ['**/*.css'], loaders: ['sass'] },
+    { builder: 'mkdist', input: '../../src/packages/use-composable', pattern: ['**/*.vue'], loaders: ['vue'] },
+    { builder: 'mkdist', input: '../../src/packages/use-composable', pattern: ['**/*.ts'], format: 'esm', loaders: ['js'] },
+  ],
+  clean: true,
+  declaration: true,
+  externals: ['vue'],
+})
