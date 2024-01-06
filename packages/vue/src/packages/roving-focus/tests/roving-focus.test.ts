0c86ea4fb872ccfc9f30b65c1dff88065664ec4d
productdevbook <hi@productdevbook.com>
2024-01-06 10:58:21 +0300

    move story and test

---
+++ packages/vue/src/packages/roving-focus/tests/roving-focus.test.ts
@@ -0,0 +1,47 @@
+import { describe, expect, it } from 'vitest'
+import { mount } from '@vue/test-utils'
+
+import type { Component } from 'vue'
+import { h } from 'vue'
+import { OkuRovingFocusGroup, OkuRovingFocusGroupItem } from '@oku-ui/roving-focus'
+
+const component = {
+  setup(props, { slots, attrs }) {
+    return () => h(OkuRovingFocusGroup, attrs, slots)
+  },
+} as Component
+
+describe('okuRovingFocusGroup', () => {
+  describe('okuRovingFocusGroupItem aschild', () => {
+    it('empty', () => {
+      const com = mount(component, {
+        slots: {
+          default: () => [
+            h(OkuRovingFocusGroupItem, {
+              asChild: true,
+            }),
+            h(OkuRovingFocusGroupItem, {
+              asChild: true,
+            }),
+          ],
+        },
+      })
+      expect(com.element).toMatchSnapshot()
+    })
+
+    it('one button', async () => {
+      const com = mount(component, {
+        slots: {
+          default: () => [
+            h(OkuRovingFocusGroupItem, {
+              asChild: true,
+            }, {
+              default: () => h('button', {}, 'button'),
+            }),
+          ],
+        },
+      })
+      expect(com.element).toMatchSnapshot()
+    })
+  })
+})
