9803c6eaf2587141fd55636b75f85e5991fa832f
productdevbook <hi@productdevbook.com>
2024-01-06 08:30:17 +0300

    move tabs

---
+++ packages/vue/package-build/tabs/build.config.ts
@@ -0,0 +1,12 @@
+import { defineBuildConfig } from 'unbuild'
+
+export default defineBuildConfig({
+  entries: [
+    { builder: 'mkdist', input: '../../src/packages/tabs', pattern: ['**/*.css'], loaders: ['sass'] },
+    { builder: 'mkdist', input: '../../src/packages/tabs', pattern: ['**/*.vue'], loaders: ['vue'] },
+    { builder: 'mkdist', input: '../../src/packages/tabs', pattern: ['**/*.ts'], format: 'esm', loaders: ['js'] },
+  ],
+  clean: true,
+  declaration: true,
+  externals: ['vue'],
+})
