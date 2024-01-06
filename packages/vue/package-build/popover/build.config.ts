4b58c19f82e4588890722b0c8bbe1478c706052b
productdevbook <hi@productdevbook.com>
2024-01-06 08:19:17 +0300

    move popover

---
+++ packages/vue/package-build/popover/build.config.ts
@@ -0,0 +1,12 @@
+import { defineBuildConfig } from 'unbuild'
+
+export default defineBuildConfig({
+  entries: [
+    { builder: 'mkdist', input: '../../src/packages/popover', pattern: ['**/*.css'], loaders: ['sass'] },
+    { builder: 'mkdist', input: '../../src/packages/popover', pattern: ['**/*.vue'], loaders: ['vue'] },
+    { builder: 'mkdist', input: '../../src/packages/popover', pattern: ['**/*.ts'], format: 'esm', loaders: ['js'] },
+  ],
+  clean: true,
+  declaration: true,
+  externals: ['vue'],
+})
