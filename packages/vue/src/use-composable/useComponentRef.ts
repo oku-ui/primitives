b9520f3ae733befa622d1f754fb99aeba6236bd5
productdevbook <hi@productdevbook.com>
2024-01-10 18:41:52 +0300

    Add useForwardRef to useComponentRef

--- packages/vue/src/use-composable/useComponentRef.ts
+++ packages/vue/src/use-composable/useComponentRef.ts
@@ -2,18 +2,21 @@
 
 import type { ComponentPublicInstance } from 'vue'
 import { computed, ref } from 'vue'
-import { unrefElement } from '.'
+import { unrefElement, useForwardRef } from '.'
 
 export function useComponentRef<T = HTMLElement>() {
   const componentRef = ref<ComponentPublicInstance>()
-
+  const forword = useForwardRef()
   const currentElement = computed<T>(() => {
-    const test = ['#comment', '#text'].includes(componentRef.value?.$el?.nodeName)
+    const el = ['#comment', '#text'].includes(componentRef.value?.$el?.nodeName)
       ? componentRef.value?.$el
       : unrefElement(componentRef)
+    if (el)
+      forword(el)
     // console.log('componentRef.value?.$el.nodeName', test)
     // console.log(componentRef.value?.$el?.nodeName)
-    return test
+
+    return el
   })
 
   return {
