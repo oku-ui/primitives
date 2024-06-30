3b05fac042bea2b9b951c14887c777828357a421
teleskop150750 <teleskop150750@gmail.com>
2024-06-30 16:44:51 +0300

    refactor: [Checkbox] rename vars

--- packages/vue-primitives/src/checkbox/BubbleInput.vue
+++ packages/vue-primitives/src/checkbox/BubbleInput.vue
@@ -16,7 +16,7 @@ const props = withDefaults(defineProps<BubbleInputProps>(), {
 const elRef = shallowRef<HTMLInputElement>()
 
 const controlSize = useSize(() => props.control)
-const checked = isIndeterminate(props.checked) ? false : props.checked
+const initChecked = isIndeterminate(props.checked) ? false : props.checked
 
 // Bubble checked change to parents (e.g form change event)
 watch(() => props.checked, (checked, prevChecked) => {
@@ -44,7 +44,7 @@ watch(() => props.checked, (checked, prevChecked) => {
     type="checkbox"
     aria-hidden
     tabindex="-1"
-    :checked="checked"
+    :checked="initChecked"
     :style="{
       width: `${controlSize?.width || 0}px`,
       height: `${controlSize?.width || 0}px`,
