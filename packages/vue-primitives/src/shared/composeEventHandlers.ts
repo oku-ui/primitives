7854baac833bc5f851252963a76c86a19efb1650
teleskop150750 <teleskop150750@gmail.com>
2024-09-19 09:26:36 +0300

    ref: [composeEventHandlers] handle disabled

--- packages/vue-primitives/src/shared/composeEventHandlers.ts
+++ packages/vue-primitives/src/shared/composeEventHandlers.ts
@@ -4,6 +4,16 @@ export function composeEventHandlers<E extends Event>(
   { checkForDefaultPrevented = true } = {},
 ) {
   return function handleEvent(event: E) {
+    const currentTarget = event.currentTarget as HTMLButtonElement
+    if (currentTarget.disabled || currentTarget.hasAttribute('data-disabled')) {
+      return
+    }
+
+    const target = event.target as HTMLButtonElement
+    if (target.disabled || target.hasAttribute('data-disabled')) {
+      return
+    }
+
     originalEventHandler?.(event)
 
     if (checkForDefaultPrevented === false || !((event as unknown) as Event).defaultPrevented)
