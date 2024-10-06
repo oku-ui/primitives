f5afd8a16a1c50da4ad94c2d71a0baf09483bb1f
teleskop150750 <teleskop150750@gmail.com>
2024-10-06 20:28:31 +0300

    wip: toolbar

--- packages/vue-primitives/src/toolbar/ToolbarToggleGroup.ts
+++ packages/vue-primitives/src/toolbar/ToolbarToggleGroup.ts
@@ -1,6 +1,8 @@
 import type { RadixPrimitiveReturns } from '../shared/typeUtils.ts'
 import type { ToggleGroupEmits, ToggleGroupProps, ToggleGroupType } from '../toggle-group/index.ts'
+import { mergePrimitiveAttrs } from '../shared/mergeProps.ts'
 import { useToggleGroup, type UseToggleGroupProps } from '../toggle-group/ToggleGroupRoot.ts'
+import { useToolbarContext } from './ToolbarRoot.ts'
 
 export interface ToolbarToggleGroupProps<T extends ToggleGroupType> extends Omit<ToggleGroupProps<T>, 'rovingFocus'> {}
 
@@ -9,6 +11,8 @@ export type ToolbarToggleGroupEmits<T extends ToggleGroupType> = ToggleGroupEmit
 export interface UseToolbarToggleGroupProps<T extends ToggleGroupType> extends Omit<UseToggleGroupProps<T>, 'rovingFocus'> {}
 
 export function useToolbarToggleGroup<T extends ToggleGroupType>(props: UseToolbarToggleGroupProps<T>): RadixPrimitiveReturns {
+  const context = useToolbarContext('ToolbarToggleGroup')
+
   const toggleGroup = useToggleGroup({
     type: props.type,
     value: props.value,
@@ -23,8 +27,12 @@ export function useToolbarToggleGroup<T extends ToggleGroupType>(props: UseToolb
   })
 
   return {
-    attrs(extraAttrs) {
-      return toggleGroup.attrs(extraAttrs)
+    attrs(extraAttrs = []) {
+      const attrs = {
+        'data-orientation': context.orientation,
+      }
+
+      return toggleGroup.attrs([attrs, ...extraAttrs])
     },
   }
 }
