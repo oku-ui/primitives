b52f9ee8f9470a1c2797c5f7c791a9dff2d37e9b
teleskop150750 <teleskop150750@gmail.com>
2024-10-06 20:07:52 +0300

    chore: merge v1

--- packages/vue-primitives/src/checkbox/stories/WithinForm.vue
+++ packages/vue-primitives/src/checkbox/stories/WithinForm.vue
@@ -1,4 +1,3 @@
-
 <script setup lang="ts">
 import { shallowRef } from 'vue'
 import { CheckboxBubbleInput, CheckboxIndicator, CheckboxRoot } from '../index.ts'
@@ -19,26 +18,50 @@ function onChange(event: Event) {
 </script>
 
 <template>
-  <form @change="onChange" @submit.prevent="() => { }">
-    <fieldset>
-      <legend>optional checked: {{ String(data.optional) }}</legend>
-      <label>
-        <CheckboxRoot v-model:checked="checked" class="checkbox_rootClass" name="optional">
+  <div>
+    <h1>TODO: Within Form</h1>
+    <form @change="onChange" @submit.prevent="() => { }">
+      <fieldset>
+        <legend>optional checked: {{ String(data.optional) }}</legend>
+        <label>
+          <CheckboxRoot v-model:checked="checked" class="checkbox_rootClass" name="optional">
+            <CheckboxBubbleInput />
+            <CheckboxIndicator class="checkbox_indicatorClass" />
+          </CheckboxRoot>{{ ' ' }}
+          with label
+        </label>
+        <br>
+        <br>
+
+        <button
+          type="button"
+          @click="() => {
+            checked = checked === 'indeterminate' ? false : 'indeterminate'
+          }"
+        >
+          Toggle indeterminate
+        </button>
+      </fieldset>
+
+      <br>
+      <br>
+
+      <fieldset>
+        <legend>required checked: {{ String(data.required) }}</legend>
+        <CheckboxRoot class="checkbox_rootClass" name="required" required>
           <CheckboxBubbleInput />
           <CheckboxIndicator class="checkbox_indicatorClass" />
-        </CheckboxRoot>{{ ' ' }}
-        with label
-      </label>
+        </CheckboxRoot>
+      </fieldset>
+
       <br>
       <br>
 
       <fieldset>
         <legend>stop propagation checked: {{ String(data.stopprop) }}</legend>
         <CheckboxRoot class="checkbox_rootClass" name="stopprop" @click="(event: Event) => event.stopPropagation()">
-          <template #default="scope">
-            <CheckboxBubbleInput v-if="scope.isFormControl" v-bind="scope.input" />
-            <CheckboxIndicator class="checkbox_indicatorClass" />
-          </template>
+          <CheckboxBubbleInput />
+          <CheckboxIndicator class="checkbox_indicatorClass" />
         </CheckboxRoot>
       </fieldset>
 
@@ -48,36 +71,7 @@ function onChange(event: Event) {
       <button type="reset">
         Reset
       </button>
-    </fieldset>
-
-    <br>
-    <br>
-
-    <fieldset>
-      <legend>required checked: {{ String(data.required) }}</legend>
-      <CheckboxRoot class="checkbox_rootClass" name="required" required>
-        <CheckboxBubbleInput />
-        <CheckboxIndicator class="checkbox_indicatorClass" />
-      </CheckboxRoot>
-    </fieldset>
-
-    <br>
-    <br>
-
-    <fieldset>
-      <legend>stop propagation checked: {{ String(data.stopprop) }}</legend>
-      <CheckboxRoot class="checkbox_rootClass" name="stopprop" @click="(event: Event) => event.stopPropagation()">
-        <CheckboxBubbleInput />
-        <CheckboxIndicator class="checkbox_indicatorClass" />
-      </CheckboxRoot>
-    </fieldset>
-
-    <br>
-    <br>
-
-    <button type="reset">
-      Reset
-    </button>
-    <button>Submit</button>
-  </form>
+      <button>Submit</button>
+    </form>
+  </div>
 </template>
--- packages/vue-primitives/src/radio-group/stories/WithinForm.vue
+++ packages/vue-primitives/src/radio-group/stories/WithinForm.vue
@@ -11,90 +11,92 @@ function setData(callback: (prevData: typeof data.value) => typeof data.value) {
 </script>
 
 <template>
-  <form
-    @submit="(event) => event.preventDefault()"
-    @change="(event) => {
-      const radio = event.target as HTMLInputElement;
-      console.log('change', radio.name, radio.value)
-      setData((prevData) => ({ ...prevData, [radio.name]: radio.value }));
-    }"
-  >
-    <fieldset>
-      <legend>optional value: {{ data.optional }}</legend>
-      <RadioGroupRoot class="radioGroup_rootClass" name="optional">
-        <RadioGroupItem class="radioGroup_itemClass" value="1">
-          <RadioGroupBubbleInput />
-          <RadioGroupIndicator class="radioGroup_indicatorClass" />
-        </RadioGroupItem>
-        <RadioGroupItem class="radioGroup_itemClass" value="2">
-          <RadioGroupBubbleInput />
-          <RadioGroupIndicator class="radioGroup_indicatorClass" />
-        </RadioGroupItem>
-        <RadioGroupItem class="radioGroup_itemClass" value="3">
-          <RadioGroupBubbleInput />
-          <RadioGroupIndicator class="radioGroup_indicatorClass" />
-        </RadioGroupItem>
-      </RadioGroupRoot>
-    </fieldset>
+  <div>
+    <h1>TODO: Within Form</h1>
+    <form
+      @submit="(event) => event.preventDefault()"
+      @change="(event) => {
+        const radio = event.target as HTMLInputElement;
+        console.log('change', radio.name, radio.value)
+        setData((prevData) => ({ ...prevData, [radio.name]: radio.value }));
+      }"
+    >
+      <fieldset>
+        <legend>optional value: {{ data.optional }}</legend>
+        <RadioGroupRoot class="radioGroup_rootClass" name="optional">
+          <RadioGroupItem class="radioGroup_itemClass" value="1">
+            <RadioGroupBubbleInput />
+            <RadioGroupIndicator class="radioGroup_indicatorClass" />
+          </RadioGroupItem>
+          <RadioGroupItem class="radioGroup_itemClass" value="2">
+            <RadioGroupBubbleInput />
+            <RadioGroupIndicator class="radioGroup_indicatorClass" />
+          </RadioGroupItem>
+          <RadioGroupItem class="radioGroup_itemClass" value="3">
+            <RadioGroupBubbleInput />
+            <RadioGroupIndicator class="radioGroup_indicatorClass" />
+          </RadioGroupItem>
+        </RadioGroupRoot>
+      </fieldset>
 
-    <br>
-    <br>
+      <br>
+      <br>
 
-    <fieldset>
-      <legend>required value: {{ data.required }}</legend>
-      <RadioGroupRoot class="radioGroup_rootClass" name="required" required>
-        <RadioGroupItem class="radioGroup_itemClass" value="1">
-          <RadioGroupBubbleInput />
-          <RadioGroupIndicator class="radioGroup_indicatorClass" />
-        </RadioGroupItem>
-        <RadioGroupItem class="radioGroup_itemClass" value="2">
-          <RadioGroupBubbleInput />
-          <RadioGroupIndicator class="radioGroup_indicatorClass" />
-        </RadioGroupItem>
-        <RadioGroupItem class="radioGroup_itemClass" value="3">
-          <RadioGroupBubbleInput />
-          <RadioGroupIndicator class="radioGroup_indicatorClass" />
-        </RadioGroupItem>
-      </RadioGroupRoot>
-    </fieldset>
+      <fieldset>
+        <legend>required value: {{ data.required }}</legend>
+        <RadioGroupRoot class="radioGroup_rootClass" name="required" required>
+          <RadioGroupItem class="radioGroup_itemClass" value="1">
+            <RadioGroupBubbleInput />
+            <RadioGroupIndicator class="radioGroup_indicatorClass" />
+          </RadioGroupItem>
+          <RadioGroupItem class="radioGroup_itemClass" value="2">
+            <RadioGroupBubbleInput />
+            <RadioGroupIndicator class="radioGroup_indicatorClass" />
+          </RadioGroupItem>
+          <RadioGroupItem class="radioGroup_itemClass" value="3">
+            <RadioGroupBubbleInput />
+            <RadioGroupIndicator class="radioGroup_indicatorClass" />
+          </RadioGroupItem>
+        </RadioGroupRoot>
+      </fieldset>
 
-    <br>
-    <br>
+      <br>
+      <br>
 
-    <fieldset>
-      <legend>stop propagation value: {{ data.stopprop }}</legend>
-      <RadioGroupRoot class="radioGroup_rootClass" name="stopprop">
-        <RadioGroupItem
-          class="radioGroup_itemClass"
-          value="1"
-          @click="(event: Event) => event.stopPropagation()"
-        >
-          <RadioGroupBubbleInput />
-          <RadioGroupIndicator class="radioGroup_indicatorClass" />
-        </RadioGroupItem>
-        <RadioGroupItem
-          class="radioGroup_itemClass"
-          value="2"
-          @click="(event: Event) => event.stopPropagation()"
-        >
-          <RadioGroupBubbleInput />
-          <RadioGroupIndicator class="radioGroup_indicatorClass" />
-        </RadioGroupItem>
-        <RadioGroupItem
-          class="radioGroup_itemClass"
-          value="3"
-          @click="(event: Event) => event.stopPropagation()"
-        >
-          <RadioGroupBubbleInput />
-          <RadioGroupIndicator class="radioGroup_indicatorClass" />
-        </RadioGroupItem>
-      </RadioGroupRoot>
-    </fieldset>
+      <fieldset>
+        <legend>stop propagation value: {{ data.stopprop }}</legend>
+        <RadioGroupRoot class="radioGroup_rootClass" name="stopprop">
+          <RadioGroupItem
+            class="radioGroup_itemClass"
+            value="1"
+            @click="(event: Event) => event.stopPropagation()"
+          >
+            <RadioGroupBubbleInput />
+            <RadioGroupIndicator class="radioGroup_indicatorClass" />
+          </RadioGroupItem>
+          <RadioGroupItem
+            class="radioGroup_itemClass"
+            value="2"
+            @click="(event: Event) => event.stopPropagation()"
+          >
+            <RadioGroupBubbleInput />
+            <RadioGroupIndicator class="radioGroup_indicatorClass" />
+          </RadioGroupItem>
+          <RadioGroupItem
+            class="radioGroup_itemClass"
+            value="3"
+            @click="(event: Event) => event.stopPropagation()"
+          >
+            <RadioGroupBubbleInput />
+            <RadioGroupIndicator class="radioGroup_indicatorClass" />
+          </RadioGroupItem>
+        </RadioGroupRoot>
+      </fieldset>
 
-    <br>
-    <br>
+      <br>
+      <br>
 
-    <button>Submit</button>
-  </form>
+      <button>Submit</button>
+    </form>
   </div>
 </template>
