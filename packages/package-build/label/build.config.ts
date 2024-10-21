4f4e6ad5c658511ee3ec5530475cd0350ee90663
productdevbook <hi@productdevbook.com>
2024-10-21 13:55:43 +0300

    chore: fix tsconfig

--- packages/package-build/label/build.config.ts
+++ packages/package-build/label/build.config.ts
@@ -4,9 +4,10 @@ export default defineBuildConfig({
   entries: [
     { builder: 'mkdist', input: '../../vue-primitives/src/label', pattern: ['**/*.css', '!stories/**', '!tests/**'], loaders: ['sass'] },
     { builder: 'mkdist', input: '../../vue-primitives/src/label', pattern: ['**/*.vue', '!stories/**', '!tests/**'], loaders: ['vue'] },
-    { builder: 'mkdist', input: '../../vue-primitives/src/label', pattern: ['**/*.ts', '!stories/**', '!tests/**'], format: 'esm', loaders: ['js'], ext: 'ts' },
+    { builder: 'mkdist', input: '../../vue-primitives/src/label', pattern: ['**/*.ts', '!stories/**', '!tests/**'], format: 'esm', ext: 'ts' },
   ],
   clean: true,
-  declaration: true,
+  declaration: false,
   externals: ['vue'],
+  failOnWarn: false,
 })
